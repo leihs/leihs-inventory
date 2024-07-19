b78fac33eef59f3600f6a7d493f9b11bd200c486
mradl <manuel.radl@zhdk.ch>
2024-07-19 20:06:57 +0200

    wip v1

--- src/leihs/inventory/server/swagger_api.clj
+++ src/leihs/inventory/server/swagger_api.clj
@@ -66,105 +66,145 @@
           end (min (+ start size) (count data))]
       (subvec data start end)))
 
-  (def app
-    (ring/ring-handler
-      (ring/router
-        [["/swagger.json"
-          {:get {:no-doc true
-                 :swagger {:info {:title "my-api"
-                                  :version "2.0.0"}}
-                 :handler (swagger/create-swagger-handler)}}]
-         ["/openapi.json"
-          {:get {:no-doc true
-                 :openapi {:openapi "3.0.0"
-                           :info {:title "my-api"
-                                  :description "openapi3-docs with reitit-http"
-                                  :version "3.0.0"}}
-                 :handler (openapi/create-openapi-handler)}}]
 
-         ["/person"
-          {:tags ["person"]}
+(defn inventory-handler [request]
+  (let [path (get-in request [:path-params :path])]
+    (if-let [resource (or (io/resource (str "public/inventory/" path))
+                        (io/resource (str "assets/" path))
+                        (io/resource (str "js/" path)))]
+      {:status 200
+       :body (slurp resource)}
+      {:status 404
+       :body "File not found"})))
 
-          ["/all"
-           {:get {:summary "Get all persons with pagination"
-                  :parameters {:query {:page ::page :size ::size}}
-                  :responses {200 {:body ::person-list}}
-                  :handler (fn [{{{:keys [page size]} :query} :parameters}]
-                             (let [paginated-data (paginate @person-db page size)]
-                               {:status 200
-                                :body paginated-data}))}}]
 
-          ["/"
-           {:post {:summary "Add a new person"
-                   :parameters {:body ::person}
-                   :responses {201 {:body ::person}}
-                   :handler (fn [{{:keys [body]} :parameters}]
-                              (add-person body)
-                              {:status 201
-                               :body body})}}]
+(defn root-handler [request]
+  (let [accept-header (get-in request [:headers "accept"])]
+    (cond
+      (clojure.string/includes? accept-header "text/html")
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       ;:body "<html><body><h1>Welcome to my API</h1></body></html>"
+       :body (slurp (io/resource "public/index.html"))
+       }
 
-          ["/{id}"
-           {:get {:summary "Get a person by ID"
-                  :parameters {:path {:id ::uuid}}
-                  :responses {200 {:body ::person}}
-                  :handler (fn [{{:keys [path]} :parameters}]
-                             (if-let [person (get-person (:id path))]
-                               {:status 200
-                                :body person}
-                               {:status 404
-                                :body {:error "Person not found"}}))}
-            :put {:summary "Update a person by ID"
-                  :parameters {:path {:id ::uuid}
-                               :body ::person}
-                  :responses {200 {:body ::person}}
-                  :handler (fn [{{:keys [path body]} :parameters}]
-                             (update-person (:id path) body)
+      (clojure.string/includes? accept-header "application/json")
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body {:message "Welcome to my API"}}
+
+      :else
+      {:status 406
+       :headers {"Content-Type" "text/plain"}
+       :body "Not Acceptable"})))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/api-docs/swagger.json"
+        {:get {:no-doc true
+               :swagger {:info {:title "my-api"
+                                :version "2.0.0"}}
+               :handler (swagger/create-swagger-handler)}}]
+       ["/api-docs/openapi.json"
+        {:get {:no-doc true
+               :openapi {:openapi "3.0.0"
+                         :info {:title "my-api"
+                                :description "openapi3-docs with reitit-http"
+                                :version "3.0.0"}}
+               :handler (openapi/create-openapi-handler)}}]
+
+       ["/person"
+        {:tags ["person"]}
+
+        ["/all"
+         {:get {:summary "Get all persons with pagination"
+                :parameters {:query {:page ::page :size ::size}}
+                :responses {200 {:body ::person-list}}
+                :handler (fn [{{{:keys [page size]} :query} :parameters}]
+                           (let [paginated-data (paginate @person-db page size)]
                              {:status 200
-                              :body body})}
-            :delete {:summary "Delete a person by ID"
-                     :parameters {:path {:id ::uuid}}
-                     :responses {204 {:description "No Content"}}
-                     :handler (fn [{{:keys [path]} :parameters}]
-                                (delete-person (:id path))
-                                {:status 204})}}]]]
+                              :body paginated-data}))}}]
 
-        {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
-         ;;:validate spec/validate ;; enable spec validation for route data
-         ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
-         :exception pretty/exception
-         :data {:coercion reitit.coercion.spec/coercion
-                :muuntaja m/instance
-                :middleware [;; swagger feature
-                             swagger/swagger-feature
-                             ;; query-params & form-params
-                             parameters/parameters-middleware
-                             ;; content-negotiation
-                             muuntaja/format-negotiate-middleware
-                             ;; encoding response body
-                             muuntaja/format-response-middleware
-                             ;; exception handling
-                             exception/exception-middleware
-                             ;; decoding request body
-                             muuntaja/format-request-middleware
-                             ;; coercing response bodys
-                             coercion/coerce-response-middleware
-                             ;; coercing request parameters
-                             coercion/coerce-request-middleware
-                             ;; multipart
-                             multipart/multipart-middleware]}
-         :conflicts nil}) ;; Ignore route conflicts
-      (ring/routes
+        ["/"
+         {:post {:summary "Add a new person"
+                 :parameters {:body ::person}
+                 :responses {201 {:body ::person}}
+                 :handler (fn [{{:keys [body]} :parameters}]
+                            (add-person body)
+                            {:status 201
+                             :body body})}}]
 
-        (swagger-ui/create-swagger-ui-handler
-          {:path "/"
-           :config {:validatorUrl nil
-                    :urls [{:name "swagger" :url "swagger.json"}
-                           {:name "openapi" :url "openapi.json"}]
-                    :urls.primaryName "openapi"
-                    :operationsSorter "alpha"}})
-        (ring/create-default-handler)
 
-        )))
+
+        ["/{id}"
+         {:get {:summary "Get a person by ID"
+                :parameters {:path {:id ::uuid}}
+                :responses {200 {:body ::person}}
+                :handler (fn [{{:keys [path]} :parameters}]
+                           (if-let [person (get-person (:id path))]
+                             {:status 200
+                              :body person}
+                             {:status 404
+                              :body {:error "Person not found"}}))}
+          :put {:summary "Update a person by ID"
+                :parameters {:path {:id ::uuid}
+                             :body ::person}
+                :responses {200 {:body ::person}}
+                :handler (fn [{{:keys [path body]} :parameters}]
+                           (update-person (:id path) body)
+                           {:status 200
+                            :body body})}
+          :delete {:summary "Delete a person by ID"
+                   :parameters {:path {:id ::uuid}}
+                   :responses {204 {:description "No Content"}}
+                   :handler (fn [{{:keys [path]} :parameters}]
+                              (delete-person (:id path))
+                              {:status 204})}}]]
+
+       ["/"
+        {:get {:handler root-handler}}]
+
+
+       ["/inventory/*path"
+        {:get {:handler inventory-handler}}]
+       ]
+
+      {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+       ;;:validate spec/validate ;; enable spec validation for route data
+       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+       :exception pretty/exception
+       :data {:coercion reitit.coercion.spec/coercion
+              :muuntaja m/instance
+              :middleware [;; swagger feature
+                           swagger/swagger-feature
+                           ;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware
+                           ;; multipart
+                           multipart/multipart-middleware]}
+       :conflicts nil}) ;; Ignore route conflicts
+    (ring/routes
+
+      (swagger-ui/create-swagger-ui-handler
+        {:path "/api-docs/"
+         :config {:validatorUrl nil
+                  :urls [{:name "swagger" :url "swagger.json"}
+                         {:name "openapi" :url "openapi.json"}]
+                  :urls.primaryName "openapi"
+                  :operationsSorter "alpha"}})
+      (ring/create-default-handler))))
 
   (defn main [& args]
     (jetty/run-jetty #'app {:port 4000, :join? false})
