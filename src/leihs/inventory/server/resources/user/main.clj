bb0b3f347efba1634091c84a140c3f53632451a3
mradl <manuel.radl@zhdk.ch>
2024-10-07 17:52:07 +0200

    cleanup

--- src/leihs/inventory/server/resources/user/main.clj
+++ src/leihs/inventory/server/resources/user/main.clj
@@ -6,109 +6,54 @@
    [leihs.inventory.server.resources.utils.request :refer [path-params]]
    [next.jdbc.sql :as jdbc]
    [ring.middleware.accept]
-   ;[honey.sql :refer [format format-expr] :rename {format sql-format}]
    [ring.util.response :refer [bad-request response status]]
    [taoensso.timbre :refer [error]]))
 
 (defn get-pools-of-user-handler [request]
-  (try (let [tx (:tx request)
-
-             user_id (-> request path-params :user_id)
-             p (println ">o> user_id" user_id (type user_id))
-
-             query (-> (sql/select :p.id :p.name :p.description)
-                     (sql/from [:direct_access_rights :d])
-                     (sql/join [:users :u] [:= :u.id :d.user_id])
-                     (sql/join [:inventory_pools :p] [:= :d.inventory_pool_id :p.id])
-                     (sql/where [:= :u.id user_id])         ; Adjusted column name here for clarity
-                     sql-format)
-
-             p (println ">o> query" query)
-             result (jdbc/query tx query)
-
-             p (println ">o> result" result)
-             ]
-
-         (response result)
-         )
-       (catch Exception e
-         (error "Failed to get pools of user" e)
-         (bad-request {:error "Failed to get pools of user" :details (.getMessage e)}))
-       )
-
-  )
-
+  (try
+    (let [tx (:tx request)
+          user_id (-> request path-params :user_id)
+          query (-> (sql/select :p.id :p.name :p.description)
+                  (sql/from [:direct_access_rights :d])
+                  (sql/join [:users :u] [:= :u.id :d.user_id])
+                  (sql/join [:inventory_pools :p] [:= :d.inventory_pool_id :p.id])
+                  (sql/where [:= :u.id user_id])
+                  sql-format)
+          result (jdbc/query tx query)]
+      (response result))
+    (catch Exception e
+      (error "Failed to get pools of user" e)
+      (bad-request {:error "Failed to get pools of user" :details (.getMessage e)}))))
 
 (defn get-user-details-handler [request]
-  (try (let [tx (:tx request)
+  (try
+    (let [tx (:tx request)
+          user_id (-> request path-params :user_id)
 
-             auth-res nil
-             token-res nil
+          user-query (-> (sql/select :u.id :u.login :u.email :u.firstname :u.lastname :u.organization :u.is_admin :u.org_id)
+                       (sql/from [:users :u])
+                       (sql/where [:= :u.id user_id])
+                       sql-format)
+          user-res (jdbc/query tx user-query)
 
+          auth-query (-> (sql/select :a.id :a.authentication_system_id :a.created_at :a.updated_at)
+                       (sql/from [:users :u])
+                       (sql/join [:authentication_systems_users :a] [:= :u.id :a.user_id])
+                       (sql/where [:= :u.id user_id])
+                       sql-format)
+          auth-res (jdbc/query tx auth-query)
 
+          token-query (-> (sql/select :t.*)
+                        (sql/from [:users :u])
+                        (sql/join [:api_tokens :t] [:= :u.id :t.user_id])
+                        (sql/where [:= :u.id user_id])
+                        sql-format)
+          token-result (jdbc/query tx token-query)
+          token-res (vec (map #(dissoc % :token_hash :user_id :description) token-result))]
 
-             user_id (-> request path-params :user_id)
-             p (println ">o> user_id" user_id (type user_id))
-
-             ;query (-> (sql/select :u.*)
-             query (-> (sql/select :u.id :u.login :u.email :u.firstname :u.lastname :u.organization :u.is_admin :u.org_id)
-                     (sql/from [:users :u])
-                     (sql/where [:= :u.id user_id])
-                     sql-format)
-
-             p (println ">o> query" query)
-             result (jdbc/query tx query)
-              user-res result
-
-             p (println ">o> result" result)
-
-
-
-             ;query (-> (sql/select :u.*)
-             query (-> (sql/select :a.id :a.authentication_system_id :a.created_at :a.updated_at)
-                     (sql/from [:users :u])
-                     (sql/join [:authentication_systems_users :a] [:= :u.id :a.user_id])
-                     (sql/where [:= :u.id user_id])
-                     sql-format)
-
-             p (println ">o> query" query)
-             result (jdbc/query tx query)
-             ;result (dissoc result [:created_at :updated_at])
-
-             auth-res result
-
-             p (println ">o> result" result)
-
-
-             query (-> (sql/select :t.*)
-             ;query (-> (sql/select :id :token_hash :token_part :account_enabled )
-                     (sql/from [:users :u])
-                     (sql/join [:api_tokens :t] [:= :u.id :t.user_id])
-                     (sql/where [:= :u.id user_id])
-                     sql-format)
-
-             p (println ">o> query" query)
-             result (jdbc/query tx query)
-             p (println ">o> result" result)
-             ;result (select-keys result [:id :token_hash :token_part :account_enabled])
-
-             ;result (dissoc result [:token_hash :user_id :description])
-
-             result (vec (map #(dissoc % :token_hash :user_id :description) result))
-
-
-             token-res result
-
-             p (println ">o> result.token" result)
-             ]
-
-         (response {:user user-res
-                    :token token-res
-                    :auth auth-res})
-         )
-       (catch Exception e
-         (error "Failed to get pools of user" e)
-         (bad-request {:error "Failed to get pools of user" :details (.getMessage e)}))
-       )
-
-  )
+      (response {:user user-res
+                 :token token-res
+                 :auth auth-res}))
+    (catch Exception e
+      (error "Failed to get pools of user" e)
+      (bad-request {:error "Failed to get pools of user" :details (.getMessage e)}))))
--- src/leihs/inventory/server/resources/user/routes.clj
+++ src/leihs/inventory/server/resources/user/routes.clj
@@ -2,142 +2,29 @@
   (:require
    [clojure.set]
    [leihs.inventory.server.resources.models.models-by-pool :refer [get-models-of-pool-handler
-                                                            create-model-handler-by-pool
-                                                            get-models-of-pool-handler
-                                                            update-model-handler-by-pool
-                                                            delete-model-handler-by-pool]]
-
-
+                                                                   create-model-handler-by-pool
+                                                                   get-models-of-pool-handler
+                                                                   update-model-handler-by-pool
+                                                                   delete-model-handler-by-pool]]
    [leihs.inventory.server.resources.utils.middleware :refer [accept-json-middleware]]
-
    [leihs.inventory.server.resources.models.main :refer [get-models-handler
                                                          create-model-handler
                                                          update-model-handler
                                                          delete-model-handler]]
-
    [leihs.inventory.server.resources.user.main :refer [get-pools-of-user-handler get-user-details-handler]]
-
    [leihs.inventory.server.utils.response_helper :as rh]
    [reitit.coercion.schema]
    [reitit.coercion.spec]
    [ring.middleware.accept]
    [schema.core :as s]))
 
-;(def schema
-;  {:id s/Uuid
-;   :type s/Str
-;   (s/optional-key :manufacturer) (s/maybe s/Str)
-;   :product s/Str
-;   (s/optional-key :version) (s/maybe s/Str)
-;   (s/optional-key :info_url) (s/maybe s/Str)
-;   (s/optional-key :rental_price) (s/maybe s/Num)
-;   (s/optional-key :maintenance_period) (s/maybe s/Int)
-;   (s/optional-key :is_package) (s/maybe s/Bool)
-;   (s/optional-key :hand_over_note) (s/maybe s/Str)
-;   (s/optional-key :description) (s/maybe s/Str)
-;   (s/optional-key :internal_description) (s/maybe s/Str)
-;   (s/optional-key :technical_detail) (s/maybe s/Str)
-;   :created_at s/Inst
-;   :updated_at s/Inst
-;   (s/optional-key :cover_image_id) (s/maybe s/Uuid)})
-
 (def schema-min
   {:id s/Uuid
    :name s/Str
-   ;:produc s/Str
-   (s/optional-key :description) (s/maybe s/Str)
-    ;(s/optional-key :version) (s/maybe s/Str)
-    ;(s/optional-key :info_url) (s/maybe s/Str)
-    ;(s/optional-key :rental_price) (s/maybe s/Num)
-    ;(s/optional-key :maintenance_period) (s/maybe s/Int)
-    ;(s/optional-key :is_package) (s/maybe s/Bool)
-    ;(s/optional-key :hand_over_note) (s/maybe s/Str)
-    ;(s/optional-key :description) (s/maybe s/Str)
-    ;(s/optional-key :internal_description) (s/maybe s/Str)
-    ;(s/optional-key :technical_detail) (s/maybe s/Str)
-    ;:created_at s/Inst
-    ;:updated_at s/Inst
-    ;(s/optional-key :cover_image_id) (s/maybe s/Uuid)
-   })
-
-;(defn accept-json-middleware [handler]
-;  (fn [request]
-;    (let [accept-header (get-in request [:headers "accept"])]
-;      (if (and accept-header (re-matches #"^.*application/json.*$" accept-header))
-;        (handler request)
-;        rh/INDEX-HTML-RESPONSE-OK))))
-;
-;(defn get-model-route []
-;  ["/models"
-;
-;   {:swagger {:conflicting true
-;              :tags ["Models"] :security []}}
-;
-;   [""
-;    {:get {:conflicting true
-;           :accept "application/json"
-;           :coercion reitit.coercion.schema/coercion
-;           :middleware [accept-json-middleware]
-;           :swagger {:produces ["application/json" "text/html"]}
-;           :handler get-models-handler
-;           :description "Get all models, default: page=1, size=10, sort_by=manufacturer-asc"
-;           :parameters {:query {(s/optional-key :page) s/Int
-;                                (s/optional-key :size) s/Int
-;                                (s/optional-key :sort_by) (s/enum :manufacturer-asc :manufacturer-desc :product-asc :product-desc)
-;                                (s/optional-key :filter_manufacturer) s/Str
-;                                (s/optional-key :filter_product) s/Str}}
-;           :responses {200 {:description "OK"
-;                            :body (s/->Either [s/Any schema])}
-;                       404 {:description "Not Found"}
-;                       500 {:description "Internal Server Error"}}}
-;
-;     :post {:summary "Create model."
-;            :accept "application/json"
-;            :coercion reitit.coercion.schema/coercion
-;            :parameters {:body schema-min}
-;            :middleware [accept-json-middleware]
-;            :handler create-model-handler
-;            :responses {200 {:description "Returns the created model."
-;                             :body s/Any}
-;                        400 {:description "Bad Request / Duplicate key value of ?product?"
-;                             :body s/Any}}}}]
-;
-;   ["/:id"
-;    {:get {:accept "application/json"
-;           :conflicting true
-;           :coercion reitit.coercion.schema/coercion
-;           :middleware [accept-json-middleware]
-;           :swagger {:produces ["application/json"]}
-;           :handler get-models-handler
-;           :parameters {:path {:id s/Uuid}}
-;           :responses {200 {:description "OK"
-;                            :body (s/->Either [s/Any schema])}
-;                       204 {:description "No Content"}
-;                       404 {:description "Not Found"}
-;                       500 {:description "Internal Server Error"}}}
-;
-;     :put {:accept "application/json"
-;           :coercion reitit.coercion.schema/coercion
-;           :parameters {:path {:id s/Uuid}
-;                        :body schema-min}
-;           :middleware [accept-json-middleware]
-;           :handler update-model-handler
-;           :responses {200 {:description "Returns the updated model."
-;                            :body s/Any}}}
-;
-;     :delete {:accept "application/json"
-;              :coercion reitit.coercion.schema/coercion
-;              :parameters {:path {:id s/Uuid}}
-;              :middleware [accept-json-middleware]
-;              :handler delete-model-handler
-;              :responses {200 {:description "Returns the deleted model."
-;                               :body s/Any}
-;                          400 {:description "Bad Request"
-;                               :body s/Any}}}}]])
+   (s/optional-key :description) (s/maybe s/Str)})
 
 (defn get-user-routes []
   ["/"
-
    {:swagger {:conflicting true
               :tags ["User"] :security []}}
 
@@ -150,12 +37,9 @@
            :parameters {:path {:user_id s/Uuid}}
            :handler get-pools-of-user-handler
            :responses {200 {:description "OK"
-                            ;:body (s/->Either [s/Any schema])}
-                            ;:body s/Any}
                             :body [schema-min]}
                        404 {:description "Not Found"}
-                       500 {:description "Internal Server Error"}
-                       }}}]
+                       500 {:description "Internal Server Error"}}}}]
 
    ["details/:user_id"
     {:get {:conflicting true
@@ -166,65 +50,6 @@
            :parameters {:path {:user_id s/Uuid}}
            :handler get-user-details-handler
            :responses {200 {:description "OK"
-                            ;:body (s/->Either [s/Any schema])}
                             :body s/Any}
-                            ;:body [schema-min]}
                        404 {:description "Not Found"}
-                       500 {:description "Internal Server Error"}
-                       }}}]
-
-   ;["/models"
-   ; [""
-   ;  {:post {:conflicting true
-   ;          :accept "application/json"
-   ;          :coercion reitit.coercion.schema/coercion
-   ;          :middleware [accept-json-middleware]
-   ;          :swagger {:produces ["application/json" "text/html"]}
-   ;          :parameters {:path {:pool_id s/Uuid}
-   ;                       :body {:product s/Str
-   ;                              :version s/Str
-   ;                              (s/optional-key :type) (s/enum "Software" "Model")
-   ;                               ;;default: Model
-   ;                              (s/optional-key :is_package) s/Bool}}
-   ;
-   ;          :handler create-model-handler-by-pool
-   ;          :responses {200 {:description "OK"
-   ;                           :body s/Any}
-   ;                      404 {:description "Not Found"}
-   ;                      500 {:description "Internal Server Error"}}}}]
-   ;
-   ; ["/:model_id"
-   ;  {:get {:accept "application/json"
-   ;         :coercion reitit.coercion.schema/coercion
-   ;         :middleware [accept-json-middleware]
-   ;         :swagger {:produces ["application/json" "text/html"]}
-   ;         :parameters {:path {:pool_id s/Uuid
-   ;                             :model_id s/Uuid}}
-   ;         :handler get-models-of-pool-handler
-   ;         :responses {200 {:description "OK"
-   ;                          :body (s/->Either [s/Any schema])}
-   ;                     404 {:description "Not Found"}
-   ;                     500 {:description "Internal Server Error"}}}
-   ;
-   ;   :put {:accept "application/json"
-   ;         :coercion reitit.coercion.schema/coercion
-   ;         :parameters {:path {:pool_id s/Uuid :model_id s/Uuid}
-   ;                      :body schema-min}
-   ;         :middleware [accept-json-middleware]
-   ;         :handler update-model-handler-by-pool
-   ;         :responses {200 {:description "Returns the updated model."
-   ;                          :body s/Any}}}
-   ;
-   ;   :delete {:accept "application/json"
-   ;            :coercion reitit.coercion.schema/coercion
-   ;            :parameters {:path
-   ;                         {:pool_id s/Uuid :model_id s/Uuid}}
-   ;            :middleware [accept-json-middleware]
-   ;            :handler delete-model-handler-by-pool
-   ;            :responses {200 {:description "Returns the deleted model."
-   ;                             :body s/Any}
-   ;                        400 {:description "Bad Request"
-   ;                             :body s/Any}}}}]]
-
-
-   ])
+                       500 {:description "Internal Server Error"}}}}]])
