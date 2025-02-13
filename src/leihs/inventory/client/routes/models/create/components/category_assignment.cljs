b454d4133a1821453438eebb227662f1e48b8e34
Jan Pistor <pistor.jan@gmail.com>
2025-02-13 17:27:37 +0100

    wip

--- src/leihs/inventory/client/routes/models/create/components/category_assignment.cljs
+++ src/leihs/inventory/client/routes/models/create/components/category_assignment.cljs
@@ -1,22 +1,84 @@
 (ns leihs.inventory.client.routes.models.create.components.category-assignment
   (:require
-   ["@/components/react/sortable-list" :refer [SortableList Draggable DragHandle]]
    ["@@/button" :refer [Button]]
-   ["@@/form" :refer [FormField FormItem FormControl FormMessage]]
-   ["@@/input" :refer [Input]]
-   ["@@/table" :refer [Table TableHeader TableRow TableHead TableBody TableCell]]
-   ["lucide-react" :refer [CirclePlus Trash]]
-   ["react-hook-form" :as hook-form]
-   [leihs.inventory.client.lib.utils :refer [cj jc]]
+   ["@@/command" :refer [Command CommandEmpty CommandInput CommandItem CommandList]]
+   ["@@/popover" :refer [Popover PopoverContent PopoverTrigger]]
+   ["lucide-react" :refer [ChevronsUpDown Check]]
+   [clojure.string :as str]
    [leihs.inventory.client.routes.models.create.context :refer [state-context]]
-   [uix.core :as uix :refer [defui $]]
+   [uix.core :as uix :refer [$ defui]]
    [uix.dom]))
 
 (defui main [{:keys [control props]}]
-  (let [{:keys [categories]} (uix/use-context state-context)]
+  (let [{:keys [categories]} (uix/use-context state-context)
+        [open set-open!] (uix/use-state false)
+        [value set-value!] (uix/use-state nil)
+        items (uix/use-memo
+               (fn []
+                 (let [flattened-items (atom [])]
+                   (letfn [(flatten-category [path items level]
+                             (doseq [item items]
+                               (if (string? item)
+                                 (swap! flattened-items conj
+                                        {:value (str/join "-" (conj path item))
+                                         :label item
+                                         :level level})
+                                 (do
+                                   (swap! flattened-items conj
+                                          {:value (str/join "-" (conj path (:subcategory item)))
+                                           :label (:subcategory item)
+                                           :level level})
+                                   (when (seq (:items item))
+                                     (flatten-category (conj path (:subcategory item)) (:items item) (inc level)))))))]
+                     (doseq [category categories]
+                       (swap! flattened-items conj
+                              {:value (:category category)
+                               :label (:category category)
+                               :level 0})
+                       (flatten-category [(:category category)] (:items category) 1))
+                     @flattened-items))) [categories])]
 
-    (js/console.debug "categories" categories)
-    ($ :div "hello")))
+    ($ Popover {:open open
+                :on-open-change #(set-open!)}
+       ($ PopoverTrigger {:as-child true}
+          ($ Button {:variant "outline"
+                     :role "combobox"
+                     :aria-expanded open
+                     :class-name "w-[300px] justify-between"}
+             (if (seq value)
+               (let [item (some #(when (= (:value %) value) %) items)]
+                 (:label item))
+               "Select item...")
+             ($ ChevronsUpDown {:class-name "ml-2 h-4 w-4 shrink-0 opacity-50"})))
+
+       ($ PopoverContent {:class-name "w-[300px] p-0"}
+          ($ Command
+             ($ CommandInput {:placeholder "Search item..."}
+                ($ CommandList
+                   ($ CommandEmpty "No item found.")
+                   ($ CommandItem "hello")))
+
+             #_($ CommandList nil
+                  ($ CommandEmpty nil "No item found.")
+                  (for [item items]
+                    ($
+                     CommandItem
+                     {:key (:value item)
+                      :value (:value item)
+                      :on-select #(let [current-value (if (= % value) "" %)]
+                                    (set-value! value current-value)
+                                    (set-open! open false))
+                      :class-name (str "pl-[" (* (:level item) 12) "px]")}
+                     ($ Check
+                        {:class-name (str "mr-2 h-4 w-4"
+                                          (if (= value (:value item))
+                                            "opacity-100"
+                                            "opacity-0"))})
+                     ($ :span
+                        {:class-name (str (when (zero? (:level item)) "font-bold")
+                                          (when (= 1 (:level item)) "font-medium")
+                                          "truncate")}
+                        (:label item))))))))))
 
 (def CategoryAssignment
   (uix/as-react
