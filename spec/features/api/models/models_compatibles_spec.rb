7b1ee43d87f5d9304a5e6e5f15d2601da0240339
mradl <manuel.radl@zhdk.ch>
2025-02-27 16:34:03 +0100

    fix v1

--- spec/features/api/models/models_compatibles_spec.rb
+++ spec/features/api/models/models_compatibles_spec.rb
@@ -10,9 +10,15 @@ feature "Inventory API Endpoints - Compatible Models" do
 
     context "GET /inventory/models-compatibles" do
       it "retrieves no compatible models and returns status 200" do
-        resp = client.get "/inventory/models-compatibles"
+        resp = client.get "/inventory/models-compatibles?page=1&size=100"
         expect(resp.status).to eq(200)
         expect(resp.body["pagination"]["total_records"]).to eq(0)
+      end
+
+      it "retrieves no compatible models and returns status 200" do
+        resp = client.get "/inventory/models-compatibles"
+        expect(resp.status).to eq(200)
+        expect(resp.body.count).to eq(0)
       end
 
       it "retrieves paginated results with no compatible models and returns status 200" do
