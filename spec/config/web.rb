a0276df8b9663a25ac66a79085ea403e7ce94112
mradl <manuel.radl@zhdk.ch>
2025-04-07 10:30:11 +0200

    login test v2

--- spec/config/web.rb
+++ spec/config/web.rb
@@ -6,14 +6,14 @@ ACCEPT_CSV = "text/csv"
 ACCEPT_HTML = "text/html"
 ACCEPT_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
-X_CSRF_TOKEN= "test-csrf-123-456"
+X_CSRF_TOKEN = "test-csrf-123-456"
 
 def login_and_extract_session_token(user)
 
-resp = basic_auth_plain_faraday_json_client(user.login, user.password).get("/inventory/login")
-expect(resp.status).to eq(200)
+  resp = basic_auth_plain_faraday_json_client(user.login, user.password).get("/inventory/login")
+  expect(resp.status).to eq(200)
 
-parse_cookie(resp.headers["set-cookie"])["leihs-user-session"]
+  parse_cookie(resp.headers["set-cookie"])["leihs-user-session"]
 
 end
 
@@ -32,7 +32,7 @@ end
 def plain_faraday_client
   @plain_faraday_client ||= Faraday.new(
     url: api_base_url,
-    headers: {accept: "*/*"}
+    headers: { accept: "*/*" }
   ) do |conn|
     yield(conn) if block_given?
     conn.adapter Faraday.default_adapter
@@ -42,7 +42,7 @@ end
 def plain_faraday_resource_client(headers = {})
   @plain_faraday_client ||= Faraday.new(
     url: api_base_url,
-    headers: {accept: "image/jpeg"}.merge(headers)
+    headers: { accept: "image/jpeg" }.merge(headers)
   ) do |conn|
     yield(conn) if block_given?
     conn.adapter Faraday.default_adapter
@@ -52,7 +52,7 @@ end
 def plain_faraday_json_client(headers = nil)
   @plain_faraday_json_client ||= Faraday.new(
     url: api_base_url,
-    headers: headers || {accept: "application/json"}
+    headers: headers || { accept: "application/json" }
   ) do |conn|
     yield(conn) if block_given?
     conn.response :json, content_type: /\bjson$/
@@ -63,7 +63,7 @@ end
 def basic_auth_plain_faraday_json_client(login, password)
   @basic_auth_plain_faraday_json_client ||= Faraday.new(
     url: api_base_url,
-    headers: {accept: "application/json"}
+    headers: { accept: "application/json" }
   ) do |conn|
     conn.request :basic_auth, login, password
     yield(conn) if block_given?
@@ -75,7 +75,7 @@ end
 def wtoken_header_plain_faraday_json_client(token)
   @plain_faraday_json_client ||= Faraday.new(
     url: api_base_url,
-    headers: {accept: "application/json", Authorization: "token #{token}"}
+    headers: { accept: "application/json", Authorization: "token #{token}" }
   ) do |conn|
     yield(conn) if block_given?
     conn.response :json, content_type: /\bjson$/
@@ -140,34 +140,57 @@ def json_client_patch(url, body: nil, headers: {}, token: nil)
   common_plain_faraday_client(:patch, url, token: token, body: body, headers: headers)
 end
 
-def session_auth_plain_faraday_json_client(cookies: nil)
-      headers = {"accept" => "application/json", "x-csrf-token" => X_CSRF_TOKEN}
-      headers[:Cookie] = cookies.map(&:to_s).join("; ") if cookies
+# def session_auth_plain_faraday_json_client(cookies: nil, headers: {})
+#       headers_default = {"accept" => "application/json", "x-csrf-token" => X_CSRF_TOKEN}
+#
+#       # headers.update(headers) if headers
+#       headers=headers_default if empty headers
+#
+#       headers[:Cookie] = cookies.map(&:to_s).join("; ") if cookies
+#
+#       @plain_faraday_json_client ||= Faraday.new(
+#         url: api_base_url,
+#         headers: headers
+#       ) do |conn|
+#         yield(conn) if block_given?
+#         conn.response :json, content_type: /\bjson$/
+#         conn.adapter Faraday.default_adapter
+#       end
+# end
 
-      @plain_faraday_json_client ||= Faraday.new(
-        url: api_base_url,
-        headers: headers
-      ) do |conn|
-        yield(conn) if block_given?
-        conn.response :json, content_type: /\bjson$/
-        conn.adapter Faraday.default_adapter
-      end
-    end
+def session_auth_plain_faraday_json_client(cookies: nil, headers: nil)
+  # Use default headers only if no headers are provided
+  # headers ||= { "accept" => "application/json", "x-csrf-token" => X_CSRF_TOKEN }
+  headers ||= { "accept" => "application/json" }
+
+  # Add cookies if provided
+  headers[:Cookie] = cookies.map(&:to_s).join("; ") if cookies
+
+  Faraday.new(url: api_base_url, headers: headers) do |conn|
+    yield(conn) if block_given?
+    conn.response :json, content_type: /\bjson$/
+    conn.adapter Faraday.default_adapter
+  end
+end
+
+def session_auth_plain_faraday_json_csrf_client(cookies: nil, headers: { "accept" => "application/json", "x-csrf-token" => X_CSRF_TOKEN })
+  session_auth_plain_faraday_json_client(cookies: cookies, headers: headers)
+end
 
 ResponseResult = Struct.new(:status, :body)
 
 # def http_multipart_client(url, form_data, method: :post, headers: {"Accept" => "application/json", "x-csrf-token" => X_CSRF_TOKEN}, token: nil)
-def http_multipart_client(url, form_data, method: :post, headers: {"Accept" => "application/json"}, token: nil)
+def http_multipart_client(url, form_data, method: :post, headers: { "Accept" => "application/json" }, token: nil)
   uri = URI.parse(api_base_url + url)
   http = Net::HTTP.new(uri.host, uri.port)
 
   request_class = case method
-  when :post then Net::HTTP::Post
-  when :put then Net::HTTP::Put
-  when :patch then Net::HTTP::Patch
-  else
-    raise ArgumentError, "Unsupported HTTP method: #{method}"
-  end
+                  when :post then Net::HTTP::Post
+                  when :put then Net::HTTP::Put
+                  when :patch then Net::HTTP::Patch
+                  else
+                    raise ArgumentError, "Unsupported HTTP method: #{method}"
+                  end
 
   request = request_class.new(uri)
   headers["Authorization"] = "Token #{token}" if token
@@ -209,10 +232,10 @@ def parse_cookie(cookie_string)
     next unless key && value
 
     cookie_hash[key] = if key == "leihs-session"
-      parse_leihs_session(value)
-    else
-      value.strip
-    end
+                         parse_leihs_session(value)
+                       else
+                         value.strip
+                       end
   end
 
   cookie_hash
--- spec/features/api/auth_session_csrf_login_spec.rb
+++ spec/features/api/auth_session_csrf_login_spec.rb
@@ -7,103 +7,109 @@ feature "Call swagger-endpoints" do
     end
 
     let(:client) { plain_faraday_json_client }
+    let(:cookie) { CGI::Cookie.new("name" => "leihs-anti-csrf-token", "value" => X_CSRF_TOKEN).to_s }
 
-
-    # TODO:
-    it "accesses protected resource with valid session cookie" do
-      resp = plain_faraday_json_client.get("/inventory/session/protected")
+    it "denies access to protected resource without login" do
+      resp = client.get("/inventory/session/protected")
       expect(resp.status).to eq(403)
-
-
-
-      # redirect after invalid login
-      resp = common_plain_faraday_login_client(:post, "/sign-in",  body: {
-        "user" => @user.login,
-        "password" => @user.password,
-        "csrf-token" => X_CSRF_TOKEN,
-        "return-to" => "/inventory/models"
-      })
-
-      expect(resp.status).to eq(302)
-      expect(resp.headers["location"]).to eq("/sign-in?return-to=%2Finventory&message=CSRF-Token/Session not valid")
-
-
-
-      # redirect after correct login
-      cookie = CGI::Cookie.new("name" => "leihs-anti-csrf-token", "value" => X_CSRF_TOKEN)
-      resp = common_plain_faraday_login_client(:post, "/sign-in",  body: {
-        "user" => @user.login,
-        "password" => @user.password,
-        "csrf-token" => X_CSRF_TOKEN,
-                     "return-to" => "/inventory/models"
-      },
-        headers: {"Cookie" => cookie.to_s}
-      )
-
-      expect(resp.status).to eq(302)
-      expect(resp.headers["location"]).to eq("/inventory/8bd16d45-056d-5590-bc7f-12849f034351/models")
-
-      resp = session_auth_plain_faraday_json_client().get("/test-csrf")
-      expect(resp.status).to eq(200)
-
-      cookie = CGI::Cookie.new("name" => "leihs-anti-csrf-token", "value" => X_CSRF_TOKEN)
-
-      resp = session_auth_plain_faraday_json_client().put("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["x-csrf-token"] = X_CSRF_TOKEN
-        req.headers["Cookie"] = "#{cookie}"
-      end
-      expect(resp.status).to eq(200)
-
-      resp = session_auth_plain_faraday_json_client().put("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["x-csrf-token"] = X_CSRF_TOKEN
-      end
-
-      expect(resp.status).to eq(404)
-      expect(resp.body["detail"]).to eq("The anti-csrf-token cookie value is not set.")
-
-      resp = session_auth_plain_faraday_json_client().put("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["Cookie"] = "#{cookie}"
-      end
-      expect(resp.body["detail"]).to eq("The x-csrf-token has not been send!")
-      expect(resp.status).to eq(404)
-
-      resp = session_auth_plain_faraday_json_client().put("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-      end
-      expect(resp.status).to eq(404)
-      expect(resp.body["detail"]).to eq("The anti-csrf-token cookie value is not set.")
-
-
-      resp = session_auth_plain_faraday_json_client().put("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["x-csrf-token"] = "not-correct-token"
-        req.headers["Cookie"] = "#{cookie}"
-      end
-      expect(resp.status).to eq(404)
-      expect(resp.body["detail"]).to eq("The x-csrf-token is not equal to the anti-csrf cookie value.")
-
-
-
-
-      resp = session_auth_plain_faraday_json_client().post("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["x-csrf-token"] = X_CSRF_TOKEN
-        req.headers["Cookie"] = "#{cookie}"
-      end
-      expect(resp.status).to eq(200)
-
-      resp = session_auth_plain_faraday_json_client().delete("/test-csrf") do |req|
-        req.headers["Content-Type"] = "application/json"
-        req.headers["x-csrf-token"] = X_CSRF_TOKEN
-        req.headers["Cookie"] = "#{cookie}"
-      end
-      expect(resp.status).to eq(200)
     end
 
+    context "login flow" do
+      it "fails with invalid CSRF token/session" do
+        resp = common_plain_faraday_login_client(:post, "/sign-in", body: {
+          "user" => @user.login,
+          "password" => @user.password,
+          "csrf-token" => X_CSRF_TOKEN,
+          "return-to" => "/inventory/models"
+        })
+        expect(resp.status).to eq(302)
+        expect(resp.headers["location"]).to eq("/sign-in?return-to=%2Finventory&message=CSRF-Token/Session not valid")
+      end
 
+      it "succeeds with valid session and CSRF token" do
+        resp = common_plain_faraday_login_client(:post, "/sign-in", body: {
+          "user" => @user.login,
+          "password" => @user.password,
+          "csrf-token" => X_CSRF_TOKEN,
+          "return-to" => "/inventory/models"
+        }, headers: { "Cookie" => cookie })
 
+        expect(resp.status).to eq(302)
+        expect(resp.headers["location"]).to match(%r{/inventory/.+/models})
+      end
+    end
+
+    context "CSRF-protected endpoints" do
+      let(:auth_client) { session_auth_plain_faraday_json_client }
+
+      it "GET /test-csrf succeeds with session" do
+        resp = auth_client.get("/test-csrf")
+        expect(resp.status).to eq(200)
+      end
+
+      it "PUT /test-csrf with token and cookie succeeds" do
+        resp = auth_client.put("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["x-csrf-token"] = X_CSRF_TOKEN
+          req.headers["Cookie"] = cookie
+        end
+        expect(resp.status).to eq(200)
+      end
+
+      it "PUT /test-csrf missing cookie returns error" do
+        resp = auth_client.put("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["x-csrf-token"] = X_CSRF_TOKEN
+        end
+        expect(resp.status).to eq(404)
+        expect(resp.body["detail"]).to eq("The anti-csrf-token cookie value is not set.")
+      end
+
+      it "PUT /test-csrf missing token returns error" do
+        resp = auth_client.put("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["Cookie"] = cookie
+        end
+
+        expect(resp.status).to eq(404)
+        expect(resp.body["detail"]).to eq("The x-csrf-token has not been send!")
+      end
+
+      it "PUT /test-csrf missing token and cookie returns error" do
+        resp = auth_client.put("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+        end
+        expect(resp.status).to eq(404)
+        expect(resp.body["detail"]).to eq("The anti-csrf-token cookie value is not set.")
+      end
+
+      it "PUT /test-csrf with mismatched token returns error" do
+        resp = auth_client.put("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["x-csrf-token"] = "not-correct-token"
+          req.headers["Cookie"] = cookie
+        end
+        expect(resp.status).to eq(404)
+        expect(resp.body["detail"]).to eq("The x-csrf-token is not equal to the anti-csrf cookie value.")
+      end
+
+      it "POST /test-csrf with valid CSRF headers succeeds" do
+        resp = auth_client.post("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["x-csrf-token"] = X_CSRF_TOKEN
+          req.headers["Cookie"] = cookie
+        end
+        expect(resp.status).to eq(200)
+      end
+
+      it "DELETE /test-csrf with valid CSRF headers succeeds" do
+        resp = auth_client.delete("/test-csrf") do |req|
+          req.headers["Content-Type"] = "application/json"
+          req.headers["x-csrf-token"] = X_CSRF_TOKEN
+          req.headers["Cookie"] = cookie
+        end
+        expect(resp.status).to eq(200)
+      end
+    end
   end
 end
