fe5ece5326ac5d8255663c7228ac5388b8205de4
Jan Pistor <pistor.jan@gmail.com>
2025-05-21 19:15:00 +0200

    wip

--- src/leihs/inventory/client/routes/models/components/category_filter.cljs
+++ src/leihs/inventory/client/routes/models/components/category_filter.cljs
@@ -13,22 +13,32 @@
    [uix.core :as uix :refer [$ defui]]))
 
 (defui file-tree-item [{:keys [name category_id children]}]
-  (if (not (seq children))
-    ($ :button {:class-name "text-sm flex items-center gap-2 pl-10 py-1"}
-       name)
+  (let [[search-params set-search-params!] (router/useSearchParams)
+        handle-select (fn [e]
+                        (let [id (.. e -currentTarget -value)]
+                          (if (= id (.. search-params (get "category_id")))
+                            (.delete search-params "category_id")
+                            (.set search-params "category_id" id))
+                          (set-search-params! search-params)))]
 
-    ($ Collapsible {:class-name "pl-4"}
-       ($ CollapsibleTrigger {:class-name "text-sm w-full group flex items-center gap-2 py-1"}
-          ($ ChevronRight {:class-name "h-4 w-4 group-data-[state=open]:rotate-90 transition-transform"})
-          ($ :span {:class-name "flex items-center gap-2"} name))
-       ($ CollapsibleContent
-          (for [child children]
-            ($ file-tree-item (merge {:key (:category_id child)}
-                                     child)))))))
+    (if (not (seq children))
+      ($ :button {:role "button"
+                  :value category_id
+                  :on-click handle-select
+                  :class-name "text-sm flex items-center gap-2 pl-10 py-1"}
+         name)
+
+      ($ Collapsible {:class-name "pl-4"}
+         ($ CollapsibleTrigger {:class-name "text-sm w-full group flex items-center gap-2 py-1"}
+            ($ ChevronRight {:class-name "h-4 w-4 group-data-[state=open]:rotate-90 transition-transform"})
+            ($ :span {:class-name "flex items-center gap-2"} name))
+         ($ CollapsibleContent
+            (for [child children]
+              ($ file-tree-item (merge {:key (:category_id child)}
+                                       child))))))))
 
 (defui main [{:keys [class-name]}]
   (let [categories (:children (:categories (router/useRouteLoaderData "models-page")))
-        [search-params set-search-params!] (router/useSearchParams)
         [t] (useTranslation)]
 
     (js/console.debug categories)
