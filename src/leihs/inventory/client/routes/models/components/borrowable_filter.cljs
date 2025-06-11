4020b7014b073fbd5d185c210cf89c9637986f5b
Jan Pistor <pistor.jan@gmail.com>
2025-06-11 13:43:45 +0200

    wip

--- src/leihs/inventory/client/routes/models/components/borrowable_filter.cljs
+++ src/leihs/inventory/client/routes/models/components/borrowable_filter.cljs
@@ -4,11 +4,15 @@
                         SelectValue]]
    ["react-i18next" :refer [useTranslation]]
    ["react-router-dom" :as router]
+   [leihs.inventory.client.routes.models.filter-reducer :as filter-reducer]
    [uix.core :as uix :refer [$ defui]]))
 
 (defui main [{:keys [class-name]}]
   (let [[search-params set-search-params!] (router/useSearchParams)
         [t] (useTranslation)
+
+        dispatch (filter-reducer/use-filter-dispatcher)
+        state (filter-reducer/use-filter-state)
 
         borrowable (js/JSON.parse (.. search-params (get "borrowable")))
         handle-borrowable (fn [value]
@@ -21,7 +25,8 @@
     ($ Select {:value borrowable
                :onValueChange handle-borrowable}
        ($ SelectTrigger {:name "borrowable"
-                         :className (str "w-[260px]" class-name)}
+                         :disabled (-> state :hidden :borrowable)
+                         :className (str "w-[260px] " class-name)}
           ($ SelectValue))
        ($ SelectContent
           ($ SelectItem {:data-test-id "all"
