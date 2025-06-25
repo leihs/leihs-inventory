9b8a8bd8d4bd0fa5c1a1fc6e575c3d1059e39c95
mradl <manuel.radl@zhdk.ch>
2025-06-25 13:16:14 +0200

    wip

--- src/leihs/inventory/server/swagger_api.clj
+++ src/leihs/inventory/server/swagger_api.clj
@@ -44,7 +44,22 @@
             [ring.util.response :as response]
             [taoensso.timbre :refer [debug error warn]]))
 
-(def middlewares [wrap-handle-coercion-error
+
+(defn wrap-rewrite-urls [handler]
+  (fn [request]
+
+
+    (handler request)
+
+
+
+    ))
+
+
+
+
+(def middlewares [
+                  wrap-handle-coercion-error
                   db/wrap-tx
                   core-routing/wrap-canonicalize-params-maps
                   muuntaja/format-middleware
