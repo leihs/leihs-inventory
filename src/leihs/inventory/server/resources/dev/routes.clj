f3198589de6f441eca426d5eb61c4020751d5cf9
mradl <manuel.radl@zhdk.ch>
2025-06-26 17:24:48 +0200

    wip v14

--- src/leihs/inventory/server/resources/dev/routes.clj
+++ src/leihs/inventory/server/resources/dev/routes.clj
@@ -112,20 +112,20 @@
      :no-doc (not APPLY_DEV_ENDPOINTS)
      }
 
-    ;; TODO: move to DEV?
-    ["/pools"
-     {:get {:conflicting true
-            :summary (i/session "Get pools of the authenticated user.")
-            :accept "application/json"
-            :coercion reitit.coercion.schema/coercion
-            :middleware [wrap-authenticate! accept-json-middleware]
-            :swagger {:produces ["application/json"]}
-            :handler get-pools-of-user-handler
-            :responses {200 {:description "OK"
-                             ;:body [schema-min] ;; FIXME
-                             }
-                        404 {:description "Not Found"}
-                        500 {:description "Internal Server Error"}}}}]
+    ;;; TODO: move to DEV?
+    ;["/pools"
+    ; {:get {:conflicting true
+    ;        :summary (i/session "Get pools of the authenticated user.")
+    ;        :accept "application/json"
+    ;        :coercion reitit.coercion.schema/coercion
+    ;        :middleware [wrap-authenticate! accept-json-middleware]
+    ;        :swagger {:produces ["application/json"]}
+    ;        :handler get-pools-of-user-handler
+    ;        :responses {200 {:description "OK"
+    ;                         ;:body [schema-min] ;; FIXME
+    ;                         }
+    ;                    404 {:description "Not Found"}
+    ;                    500 {:description "Internal Server Error"}}}}]
 
     ["/update-accounts" {:put {:conflicting true
                                :summary "Overwrite pw for accounts with various roles OR is_admin"
