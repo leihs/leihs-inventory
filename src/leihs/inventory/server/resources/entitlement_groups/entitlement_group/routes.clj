6a1772ca972bd7f0aa1c070f0b7aa78b67fd4023
mradl <manuel.radl@zhdk.ch>
2025-06-27 11:18:51 +0200

    entitlement-groups

--- src/leihs/inventory/server/resources/entitlement_groups/entitlement_group/routes.clj
+++ src/leihs/inventory/server/resources/entitlement_groups/entitlement_group/routes.clj
@@ -2,20 +2,11 @@
   (:require
    [clojure.set]
    [leihs.inventory.server.resources.entitlement-groups.entitlement-group.main :refer [
-                                                                                       ;get-model-groups-of-pool-handler
-                                                             ;get-groups-of-pool-handler
                                                              get-entitlement-groups-of-pool-handler
-                                                             ;get-model-group-links-of-pool-handler
                                                              ]]
-   ;[leihs.inventory.server.resources.models.main :refer [get-models-handler
-   ;                                                      create-model-handler
-   ;                                                      update-model-handler
-   ;                                                      delete-model-handler]]
-   ;[leihs.inventory.server.resources.models.models-by-pool :refer [get-models-of-pool-handler
-   ;                                                                create-model-handler-by-pool
-   ;                                                                get-models-of-pool-handler
-   ;                                                                update-model-handler-by-pool
-   ;                                                                delete-model-handler-by-pool]]
+
+   [leihs.inventory.server.resources.entitlement-groups.types :refer [response-body]]
+
    [leihs.inventory.server.resources.utils.middleware :refer [accept-json-middleware]]
    [leihs.inventory.server.utils.response_helper :as rh]
    [reitit.coercion.schema]
@@ -33,27 +24,6 @@
     {:swagger {:conflicting true
                :tags ["Categories / Model-Groups"]}}
 
-    ;
-    ;["/ entitlement-groups"
-    ; ["" {:get {:conflicting true
-    ;            :summary "OK | a.k.a 'Anspruchsgruppen' [fe]"
-    ;            :description (create-description "https://staging.leihs.zhdk.ch/manage/8bd16d45-056d-5590-bc7f-12849f034351/groups")
-    ;            :accept "application/json"
-    ;            :coercion reitit.coercion.schema/coercion
-    ;            :middleware [accept-json-middleware]
-    ;            :swagger {:produces ["application/json"]}
-    ;            :parameters {:path {:pool_id s/Uuid}}
-    ;            :handler get-entitlement-groups-of-pool-handler
-    ;            :responses {200 {:description "OK"
-    ;                             :body [{:id s/Uuid
-    ;                                     :name s/Str
-    ;                                     :inventory_pool_id s/Uuid
-    ;                                     :is_verification_required s/Bool
-    ;                                     :created_at s/Any
-    ;                                     :updated_at s/Any}]}
-    ;                        404 {:description "Not Found"}
-    ;                        500 {:description "Internal Server Error"}}}}]
-
      ["/entitlement-groups/:entitlement_group_id"
       {:get {:conflicting true
              :summary "OK | a.k.a 'Anspruchsgruppen'"
@@ -64,14 +34,8 @@
              :parameters {:path {:pool_id s/Uuid :entitlement_group_id s/Uuid}}
              :handler get-entitlement-groups-of-pool-handler
              :responses {200 {:description "OK"
-                              :body [{:id s/Uuid
-                                      :name s/Str
-                                      :inventory_pool_id s/Uuid
-                                      :is_verification_required s/Bool
-                                      :created_at s/Any
-                                      :updated_at s/Any}]}
+                              :body [response-body]}
                          404 {:description "Not Found"}
                          500 {:description "Internal Server Error"}}}}]
 
-    ;]
    ]])
