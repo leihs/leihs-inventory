ba7b3a129490c963366da167615cbce8626029fb
mradl <manuel.radl@zhdk.ch>
2025-07-10 18:07:40 +0200

    fix v3

--- spec/backend/api/models/models_image_thumb_spec.rb
+++ spec/backend/api/models/models_image_thumb_spec.rb
@@ -79,8 +79,9 @@ describe "Swagger Inventory Endpoints - Models" do
 
           expect(resp.status).to eq(200)
           expect(resp.body["data"][0]["id"]).to eq(@model.id)
+          
           expect(resp.body["data"][0]).not_to have_key("image_url")
-          expect(resp.body["data"][0]["thumbnail_url"]).to end_with("#{@thumbnail.id}/thumbnail")
+          expect(resp.body["data"][0]["url"]).to end_with(@image.id)
           expect(resp.body["data"].count).to eq(1)
         end
 
@@ -90,9 +91,9 @@ describe "Swagger Inventory Endpoints - Models" do
           resp = client.get url
 
           expect(resp.status).to eq(200)
+          
           expect(resp.body["data"][0]["id"]).to eq(@model.id)
-          expect(resp.body["data"][0]["image_url"]).to end_with(@image.id)
-          expect(resp.body["data"][0]["thumbnail_url"]).to end_with("#{@thumbnail.id}/thumbnail")
+          expect(resp.body["data"][0]["url"]).to end_with(@image.id)
           expect(resp.body["data"].count).to eq(1)
         end
 
@@ -102,10 +103,9 @@ describe "Swagger Inventory Endpoints - Models" do
           resp = client.get url
 
           expect(resp.status).to eq(200)
+
           expect(resp.body["data"][0]["id"]).to eq(@model.id)
-          expect(resp.body["data"][0]["image_url"]).to end_with(@image2.id)
-          # FIXME: wrong thumb will be returned here, should be @thumbnail3
-          # expect(resp.body["data"][0]["cover_image_thumb"]).to end_with("#{@thumbnail3.id}/thumbnail")
+          expect(resp.body["data"][0]["url"]).to end_with(@image2.id)
           expect(resp.body["data"].count).to eq(1)
         end
       end
