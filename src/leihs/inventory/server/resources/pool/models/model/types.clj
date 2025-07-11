47e08cfd1e8fb0ec64a3360a00340bd14007db9b
mradl <manuel.radl@zhdk.ch>
2025-07-11 14:26:02 +0200

    fix v2

--- src/leihs/inventory/server/resources/pool/models/model/types.clj
+++ src/leihs/inventory/server/resources/pool/models/model/types.clj
@@ -1,6 +1,6 @@
 (ns leihs.inventory.server.resources.pool.models.model.types
   (:require
-   ;[clojure.spec.alpha :as sa]
+   [clojure.spec.alpha :as sa]
    ;[clojure.string :as str]
    [reitit.coercion.schema]
    [reitit.coercion.spec :as spec]
@@ -12,11 +12,44 @@
 
 (def put-response :model-optional-response/inventory-model)
 
-(s/defschema delete-response {:deleted_attachments [{:id uuid?
-                                                     :model_id uuid?
-                                                     :filename string?
-                                                     :size number?}]
-                              :deleted_images [any?]
-                              :deleted_model [{:id uuid?
-                                               :product string?
-                                               :manufacturer any?}]})
+;(s/def ::deleted_model
+;  (s/keys :req-un [::id ::product]
+;    :opt-un [::manufacturer]))
+;
+;(s/def ::deleted_models (s/coll-of ::deleted_model))
+;
+;(s/defschema delete-response
+;  {:deleted_attachments [{:id uuid?
+;                          :model_id uuid?
+;                          :filename string?
+;                          :size number?}]
+;   :deleted_images [any?]
+;   ;:deleted_model
+;   ::deleted_model}
+;  ;::deleted_model
+;  )
+
+
+(sa/def ::id uuid?)
+(sa/def ::model_id uuid?)
+(sa/def ::product string?)
+(sa/def ::filename  string?)
+(sa/def ::manufacturer  string?)
+(sa/def ::size  int?)
+
+(def delete-response
+
+;(sa/def ::delete-response
+  (sa/keys :req-un
+    [::deleted_attachments ::deleted_images ::deleted_model]))
+
+(sa/def ::deleted_attachments
+  (sa/coll-of
+    (sa/keys :req-un [::id ::model_id ::filename ::size])))
+
+(sa/def ::deleted_images (sa/coll-of any?))
+
+(sa/def ::deleted_model
+  (sa/coll-of
+    (sa/keys :req-un [::id ::product]
+      :opt-un [::manufacturer])))
