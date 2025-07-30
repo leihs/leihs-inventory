223a01a94f866b338cb7a1b739ad28faf20fe7ab
mradl <manuel.radl@zhdk.ch>
2025-07-30 10:15:16 +0200

    chore: add url

--- src/leihs/inventory/server/resources/pool/common.clj
+++ src/leihs/inventory/server/resources/pool/common.clj
@@ -6,6 +6,8 @@
    [clojure.string :as str]
    [honey.sql :refer [format] :rename {format sql-format}]
    [honey.sql.helpers :as sql]
+   [leihs.inventory.server.resources.pool.models.basic_coercion :as co]
+   [leihs.inventory.server.resources.pool.models.common :refer [filter-and-coerce-by-spec]]
    [leihs.inventory.server.utils.converter :refer [to-uuid]]
    [next.jdbc :as jdbc]
    [taoensso.timbre :refer [error]])
@@ -212,3 +214,16 @@
 (defn create-validation-response [data validation]
   {:data data
    :validation validation})
+
+(defn select-entries [tx table columns where-clause]
+  (jdbc/execute! tx
+                 (-> (apply sql/select columns)
+                     (sql/from table)
+                     (sql/where where-clause)
+                     sql-format)))
+
+(defn fetch-attachments [tx model-id pool-id]
+  (let [attachments (->> (select-entries tx :attachments [:id :filename :content_type] [:= :model_id model-id])
+                         (map #(assoc % :url (str "/inventory/" pool-id "/models/" model-id "/attachments/" (:id %))
+                                      :content_type (:content_type %))))]
+    (filter-and-coerce-by-spec attachments ::co/attachment)))
