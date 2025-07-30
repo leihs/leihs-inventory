e18c7ca46a6971c4bf70e848c70dd7adfa09e98c
Jan Pistor <pistor.jan@gmail.com>
2025-07-30 16:27:13 +0200

    feat: add tests

--- spec/features/software/create_spec.rb
+++ spec/features/software/create_spec.rb
@@ -8,7 +8,7 @@ feature "Create model", type: :feature do
   let(:product) { Faker::Commerce.product_name }
   let(:version) { Faker::Commerce.color }
   let(:manufacturer) { Faker::Company.name }
-  let(:technical_details) { Faker::Lorem.paragraph }
+  let(:software_information) { Faker::Lorem.paragraph }
 
   let(:attachment_name_1) { "secd.pdf" }
   let(:attachment_name_2) { "shenpaper.pdf" }
@@ -28,14 +28,15 @@ feature "Create model", type: :feature do
 
     fill_in "Product", with: product
     fill_in "Version", with: version
-    fill_in "Technical Details", with: technical_details
+    fill_in "Manufacturer", with: manufacturer
+    fill_in "Software information", with: software_information
 
-    within id: "pool.model.attachments.title" do
+    within id: "pool.software.attachments.title" do
       find("input[type='file']", visible: false).attach_file "./spec/files/#{attachment_name_1}"
       find("input[type='file']", visible: false).attach_file "./spec/files/#{attachment_name_2}"
     end
 
-    click_on "Create"
+    click_on "Save software"
 
     expect(page).to have_content "Inventory List"
     expect(page).to have_content "#{product} #{version}"
@@ -45,9 +46,10 @@ feature "Create model", type: :feature do
 
     assert_field("Product", product)
     assert_field("Version", version)
-    assert_field("Technical Details", technical_details)
+    expect(find_field("Manufacturer").value).to eq manufacturer
+    assert_field("Software information", software_information)
 
-    within id: "pool.model.attachments.title" do
+    within id: "pool.software.attachments.title" do
       find("tr", text: attachment_name_1)
       find("tr", text: attachment_name_2)
     end
