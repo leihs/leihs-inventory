973f087eb01f5bba3aa3805c6d66f65ae56c4fa0
mradl <manuel.radl@zhdk.ch>
2025-07-31 13:27:57 +0200

    wip

--- src/leihs/inventory/server/constants.clj
+++ src/leihs/inventory/server/constants.clj
@@ -1,7 +1,7 @@
 (ns leihs.inventory.server.constants)
 
 (def ACTIVATE-DEV-MODE-REDIRECT true)
-(def ACTIVATE-CSRF false)
+(def ACTIVATE-CSRF true)
 (def APPLY_API_ENDPOINTS_NOT_USED_IN_FE true)
 (def APPLY_DEV_ENDPOINTS false)
 (def HIDE_BASIC_ENDPOINTS
