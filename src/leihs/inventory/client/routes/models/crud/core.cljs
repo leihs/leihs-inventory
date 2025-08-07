651e3eced80b42650a0d24404db7a901bff0544e
Jan Pistor <pistor.jan@gmail.com>
2025-08-07 11:35:44 +0200

    chore: cleanup

--- src/leihs/inventory/client/routes/models/crud/core.cljs
+++ src/leihs/inventory/client/routes/models/crud/core.cljs
@@ -1,6 +1,5 @@
 (ns leihs.inventory.client.routes.models.crud.core
   (:require
-   [leihs.inventory.client.lib.client :refer [http-client]]
    [leihs.inventory.client.lib.form-helper :as form-helper]
    [leihs.inventory.client.lib.utils :refer [cj jc]]))
 
