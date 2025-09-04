f1061f9947229d875a5c6757f78248007bf19aff
mradl <manuel.radl@zhdk.ch>
2025-09-04 16:25:43 +0200

    merge factory

--- spec/factories/template_factory.rb
+++ spec/factories/template_factory.rb
@@ -1,3 +1,17 @@
+class Template < Sequel::Model(:model_groups)
+  many_to_many :direct_models,
+               class: :LeihsModel,
+               left_key: :model_group_id,
+               right_key: :model_id,
+               join_table: :model_links
+
+  many_to_many :inventory_pools,
+               class: :InventoryPool,
+               left_key: :model_group_id,
+               right_key: :inventory_pool_id,
+               join_table: :inventory_pools_model_groups
+end
+
 FactoryBot.define do
   factory :template do
     name { Faker::Commerce.department(max: 2) }
