30e8baafda3ef0aa0b669500b85a1fe56c39c6f4
Jan Pistor <pistor.jan@gmail.com>
2025-09-10 17:39:54 +0200

    refactor: some additions in list and add query params for model items request

---
+++ src/leihs/inventory/client/routes/pools/inventory/list/components/table/item_info.cljs
@@ -0,0 +1,28 @@
+(ns leihs.inventory.client.routes.pools.inventory.list.components.table.item-info
+  (:require
+   ["date-fns" :refer [format]]
+   ["react-i18next" :refer [useTranslation]]
+   [uix.core :as uix :refer [$ defui]]))
+
+(defui main [{:keys [item]}]
+  (let [[t] (useTranslation)]
+    ($ :div {:class-name "flex flex-row items-center"}
+       ($ :span {:class-name "w-32"}
+          (:inventory_code item))
+       ($ :div {:className "flex flex-col text-sm text-muted-foreground"}
+          ($ :span
+             (:inventory_pool_name item))
+          ($ :span
+             (if (:reservation_user_name item)
+               (str (:reservation_user_name item) " until "
+                    (format (:reservation_end_date item) "dd.MM.yyyy"))
+
+               (str (:building_name item)
+                    " ( " (:building_code item) " ) "
+                    " - " (t "pool.models.list.shelf") " "
+                    (:shelf item))))))))
+
+(def ItemInfo
+  (uix/as-react
+   (fn [props]
+     (main props))))
