e88a4c88c9ff1d39d71b4e9be6cbf9f40db53a57
Jan Pistor <pistor.jan@gmail.com>
2025-10-01 15:27:17 +0200

    fix: schemas

--- src/leihs/inventory/server/resources/pool/items/routes.clj
+++ src/leihs/inventory/server/resources/pool/items/routes.clj
@@ -1,7 +1,7 @@
 (ns leihs.inventory.server.resources.pool.items.routes
   (:require
    [leihs.inventory.server.resources.pool.items.main :as items]
-   [leihs.inventory.server.resources.pool.items.types :refer [query-params]]
+   [leihs.inventory.server.resources.pool.items.types :as types]
    [leihs.inventory.server.utils.middleware :refer [accept-json-middleware]]
    [reitit.coercion.schema]
    [reitit.coercion.spec]
@@ -10,17 +10,15 @@
 
 (defn routes []
   ["/items/"
-   {:get {:description "https://staging.leihs.zhdk.ch/manage/8bd16d45-056d-5590-bc7f-12849f034351/items"
-
-          :accept "application/json"
+   {:get {:accept "application/json"
           :coercion reitit.coercion.schema/coercion
           :middleware [accept-json-middleware]
           :swagger {:produces ["application/json"]}
-          :parameters {:path {:pool_id s/Uuid}
-                       :query query-params}
+          :summary "Returns all items/packages of a pool filtered by query parameters"
+          :parameters {:path types/path-params
+                       :query types/query-params}
           :handler items/index-resources
           :responses {200 {:description "OK"
-                           :body s/Any}
-                        ;:body get-items-response} ;; FIXME broken
+                           :body types/get-items-response}
                       404 {:description "Not Found"}
                       500 {:description "Internal Server Error"}}}}])
