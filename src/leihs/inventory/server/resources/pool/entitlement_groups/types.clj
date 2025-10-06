22a463b578fcc87b912440c2d183491ba9278319
mradl <manuel.radl@zhdk.ch>
2025-10-06 15:24:37 +0200

    chore: entitlement-groups

--- src/leihs/inventory/server/resources/pool/entitlement_groups/types.clj
+++ src/leihs/inventory/server/resources/pool/entitlement_groups/types.clj
@@ -1,10 +1,57 @@
 (ns leihs.inventory.server.resources.pool.entitlement-groups.types
   (:require
+   [leihs.inventory.server.resources.types :refer [pagination]]
    [schema.core :as s]))
 
-(def response-body {:id s/Uuid
-                    :name s/Str
-                    :inventory_pool_id s/Uuid
-                    :is_verification_required s/Bool
-                    :created_at s/Any
-                    :updated_at s/Any})
+(def PosInt
+  (s/constrained s/Int pos? 'positive-integer))
+
+(def model {:id s/Uuid
+            :model_id s/Uuid
+            :entitlement_group_id s/Uuid
+            :quantity PosInt
+            :position s/Int})
+
+(def get-model {:id s/Uuid
+                :model_id s/Uuid
+                :name s/Str
+                :quantity PosInt
+                :available_count s/Int
+                :is_quantity_ok s/Bool})
+
+(def user {:id s/Uuid
+           :user_id s/Uuid
+           :entitlement_group_id s/Uuid})
+
+(def group {:id s/Uuid
+            :group_id s/Uuid
+            :entitlement_group_id s/Uuid
+            :created_at s/Any
+            :updated_at s/Any})
+
+(def post-response-body {:entitlement_group {:id s/Uuid
+                                             :name s/Str
+                                             :inventory_pool_id s/Uuid
+                                             :is_verification_required s/Bool
+                                             :created_at s/Any
+                                             :updated_at s/Any}
+                         :models [model]
+                         :users {:deleted [user]
+                                 :created [user]}
+                         :groups {:deleted [group]
+                                  :created [group]}})
+
+(def get-response {:id s/Uuid
+                   :name s/Str
+                   :inventory_pool_id s/Uuid
+                   :is_verification_required s/Bool
+                   :created_at s/Any
+                   :updated_at s/Any
+
+                   :number_of_models s/Int
+                   :number_of_groups s/Int
+                   :number_of_direct_users s/Int
+                   :number_of_users s/Int})
+
+(def get-response-body
+  (s/->Either [[get-response] {:data [get-response] :pagination pagination}]))
---
+++ src/leihs/inventory/server/resources/pool/groups/main.clj
@@ -0,0 +1,29 @@
+(ns leihs.inventory.server.resources.pool.groups.main
+  (:require
+   [clojure.set]
+   [honey.sql.helpers :as sql]
+   [leihs.inventory.server.utils.exception-handler :refer [exception-handler]]
+   [leihs.inventory.server.utils.helper :refer [log-by-severity]]
+   [leihs.inventory.server.utils.pagination :refer [create-pagination-response]]
+   [leihs.inventory.server.utils.request-utils :refer [query-params]]
+   [ring.middleware.accept]
+   [ring.util.response :refer [response]]))
+
+(def ERROR_GET "Failed to get groups")
+
+(defn index-resources [request]
+  (try
+    (let [{:keys [search]} (-> request query-params)
+          base-query (-> (sql/select :g.id
+                                     :g.name
+                                     :g.searchable
+                                     [[:count :gu.user_id] :user_count])
+                         (sql/from [:groups :g])
+                         (sql/join [:groups_users :gu] [:= :g.id :gu.group_id])
+                         (sql/group-by :g.id :g.name :g.searchable)
+                         (cond-> search
+                           (sql/where [:ilike :g.name (str "%" search "%")])))]
+      (response (create-pagination-response request base-query nil)))
+    (catch Exception e
+      (log-by-severity ERROR_GET e)
+      (exception-handler request ERROR_GET e))))
