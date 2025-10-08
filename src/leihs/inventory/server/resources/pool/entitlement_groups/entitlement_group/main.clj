8dceede92ac104d9ac7a7734f6351144853bc98d
mradl <manuel.radl@zhdk.ch>
2025-10-08 09:53:02 +0200

    wip v8

--- src/leihs/inventory/server/resources/pool/entitlement_groups/entitlement_group/main.clj
+++ src/leihs/inventory/server/resources/pool/entitlement_groups/entitlement_group/main.clj
@@ -1,125 +1,102 @@
 (ns leihs.inventory.server.resources.pool.entitlement-groups.entitlement-group.main
   (:require
    [clojure.set]
+   [clojure.string :as str]
    [honey.sql :refer [format] :rename {format sql-format}]
    [honey.sql.helpers :as sql]
    [leihs.inventory.server.utils.exception-handler :refer [exception-handler]]
    [leihs.inventory.server.utils.request-utils :refer [path-params]]
-   ;[next.jdbc.sql :as jdbc]
-   ;[java.util UUID]
    [next.jdbc :as jdbc]
-   [ring.middleware.accept]
    [ring.util.response :refer [response]]
    [taoensso.timbre :refer [debug error]])
-(:import
- [java.util UUID]))
+  (:import [java.util UUID]))
 
-;; FIXME: 2 is buggy
-;(defn prep-query [ids]
-;          (-> (sql/select
-;                :g.name
-;                :g.is_verification_required
-;                [[:coalesce :m.number_of_models 0] :number_of_models])
-;
-;            (sql/from [:entitlement_groups :g])
-;
-;            ;; 1️⃣ Models subquery
-;            (sql/left-join
-;              [(-> (sql/select
-;                     :entitlement_group_id
-;                     [[:count :*] :number_of_models])
-;                 (sql/from :entitlements)
-;                 (sql/group-by :entitlement_group_id))
-;               :m]
-;              [:= :m.entitlement_group_id :g.id])
-;
-;            ;; 2️⃣ Users + Direct Users subquery (bug workaround)
-;            (sql/left-join
-;              [(-> (sql/select
-;                     :entitlement_group_id
-;                     [[:count :*] :number_of_users]
-;                     [[:sum
-;                       [:case
-;                        [:when
-;                         [:or
-;                          [:= :type "direct_entitlement"]
-;                          [:= :type "mixed"]]
-;                         1]
-;                        :else 0]]
-;                      :number_of_direct_users])
-;                 (sql/from :entitlement_groups_user)
-;                 (sql/group-by :entitlement_group_id))
-;               :u]
-;              [:= :u.entitlement_group_id :g.id])
-;
-;            (sql/limit 1)
-;
-;            sql-format))
+;; -----------------------------
+;; Utility functions
+;; -----------------------------
 
+(defn to-uuid [x]
+  (cond
+    (nil? x) nil
+    (uuid? x) x
+    (string? x) (UUID/fromString x)
+    (sequential? x) (mapv to-uuid x)
+    :else (throw (ex-info "Unsupported type for uuid conversion"
+                   {:value x :type (type x)}))))
 
+(def ERROR_GET "Failed to get entitlement-groups")
+
+(defn merge-by-id
+  "Merge two vectors of maps by matching :id.
+   Fields from v2 override those from v1 on collision."
+  [v1 v2]
+  (let [m2 (into {} (map (juxt :id identity) v2))]
+    (mapv #(merge % (get m2 (:id %))) v1)))
+
+(defn join-by
+  "Left-join two collections of maps by shared key k.
+   Keeps all maps from coll-a, merging matches from coll-b."
+  [k coll-a coll-b]
+  (let [b-index (into {} (map (juxt k identity)) coll-b)]
+    (mapv #(merge % (get b-index (get % k))) coll-a)))
+
+(defn add-allocation-considered-count
+  "Adds computed fields:
+   :available_count = items_count - allocations_in_other_entitlement_groups
+   :is_quantity_ok = quantity <= available_count
+   Also removes some transient keys."
+  [entitlements]
+  (mapv (fn [e]
+          (let [available (- (:items_count e 0)
+                            (:allocations_in_other_entitlement_groups e 0))
+                e (assoc e
+                    :available_count available
+                    :is_quantity_ok (<= (:quantity e 0) available))]
+            (dissoc e :entitlement_group_id :allocations_in_other_entitlement_groups)))
+    entitlements))
+
+;; -----------------------------
+;; Queries
+;; -----------------------------
 
 (defn prep-query [ids]
   (sql-format
     {:raw
      (str
        "SELECT
-           eg.id,
-           eg.name,
-           eg.is_verification_required,
-           COALESCE(m.number_of_models, 0) AS number_of_models,
-           COALESCE(u.number_of_users, 0) AS number_of_users,
-           COALESCE(u.number_of_direct_users, 0) AS number_of_direct_users,
-           COALESCE(g.number_of_groups, 0) AS number_of_groups
-        FROM entitlement_groups eg
-        LEFT JOIN (
-            SELECT entitlement_group_id, COUNT(id) AS number_of_models
-            FROM entitlements
-            GROUP BY entitlement_group_id
-        ) m ON m.entitlement_group_id = eg.id
-        LEFT JOIN (
-            SELECT entitlement_group_id,
-                   COUNT(id) AS number_of_users,
-                   SUM(CASE WHEN type IN ('direct_entitlement', 'mixed')
-                            THEN 1 ELSE 0 END) AS number_of_direct_users
-            FROM entitlement_groups_users
-            GROUP BY entitlement_group_id
-        ) u ON u.entitlement_group_id = eg.id
-        LEFT JOIN (
-            SELECT entitlement_group_id, COUNT(id) AS number_of_groups
-            FROM entitlement_groups_groups
-            GROUP BY entitlement_group_id
-        ) g ON g.entitlement_group_id = eg.id
-        WHERE eg.id IN ("
-       (->> ids
-         (map #(str "'" % "'"))
-         (clojure.string/join ", "))
+            eg.id,
+            eg.name,
+            eg.is_verification_required,
+            COALESCE(m.number_of_models, 0) AS number_of_models,
+            COALESCE(u.number_of_users, 0) AS number_of_users,
+            COALESCE(u.number_of_direct_users, 0) AS number_of_direct_users,
+            COALESCE(g.number_of_groups, 0) AS number_of_groups
+         FROM entitlement_groups eg
+         LEFT JOIN (
+             SELECT entitlement_group_id, COUNT(id) AS number_of_models
+             FROM entitlements
+             GROUP BY entitlement_group_id
+         ) m ON m.entitlement_group_id = eg.id
+         LEFT JOIN (
+             SELECT entitlement_group_id,
+                    COUNT(id) AS number_of_users,
+                    SUM(CASE WHEN type IN ('direct_entitlement', 'mixed')
+                             THEN 1 ELSE 0 END) AS number_of_direct_users
+             FROM entitlement_groups_users
+             GROUP BY entitlement_group_id
+         ) u ON u.entitlement_group_id = eg.id
+         LEFT JOIN (
+             SELECT entitlement_group_id, COUNT(id) AS number_of_groups
+             FROM entitlement_groups_groups
+             GROUP BY entitlement_group_id
+         ) g ON g.entitlement_group_id = eg.id
+         WHERE eg.id IN ("
+       (->> ids (map #(str "'" % "'")) (str/join ", "))
        ");")}))
 
-
-
-(defn to-uuid [x]
-  (cond
-    (nil? x) nil
-
-    (uuid? x) x
-
-    (string? x) (UUID/fromString x)
-
-    (sequential? x) (mapv to-uuid x)
-
-    :else (throw (ex-info "Unsupported type for uuid conversion"
-                   {:value x
-                    :type (type x)}))))
-
-(def ERROR_GET "Failed to get entitlement-groups")
-
-;(ns your-app.db.entitlements
-;  (:require
-;   [next.jdbc :as jdbc]
-;   [honeysql.helpers :as sql]
-;   [honeysql.format :as fmt]))
-
-(defn select-entitlements-with-item-count [ds inventory-pool-id model-ids exclude-group-id]
+(defn select-entitlements-with-item-count
+  "Selects entitlements with corresponding item counts for given pool and models."
+  [ds inventory-pool-id model-ids exclude-group-id]
   (let [subquery
         {:select [[[:count :*] :count]]
          :from [[:items :i]]
@@ -130,122 +107,50 @@
                  [:= :i.is_borrowable true]
                  [:is :i.parent_id nil]]}
 
-        query
-        (-> (sql/select
-              ;:e.id
-              :e.model_id
-              ;:e.entitlement_group_id
-              [:e.quantity :allocations_in_other_entitlement_groups]
-              [[subquery] :items_count])
-          (sql/from [:entitlements :e])
-          (sql/join [:entitlement_groups :eg]
-            [:= :eg.id :e.entitlement_group_id])
-          (sql/where [:and
-                      [:= :eg.inventory_pool_id inventory-pool-id]
-                      ;[:= :e.model_id model-id]
-                      ;[:in :e.model_id model-ids]
-                      [:in :e.model_id model-ids]
-                      [:!= :e.entitlement_group_id exclude-group-id]])
-          ;(sql/limit 1)
-          sql-format)]
-
+        query (-> (sql/select
+                    :e.model_id
+                    [:e.quantity :allocations_in_other_entitlement_groups]
+                    [[subquery] :items_count])
+                (sql/from [:entitlements :e])
+                (sql/join [:entitlement_groups :eg]
+                  [:= :eg.id :e.entitlement_group_id])
+                (sql/where [:and
+                            [:= :eg.inventory_pool_id inventory-pool-id]
+                            [:in :e.model_id model-ids]
+                            [:!= :e.entitlement_group_id exclude-group-id]])
+                sql-format)]
     (jdbc/execute! ds query)))
 
-
-(defn merge-by-id
-  "Merge two vectors of maps by matching :id.
-   Fields from v2 override those from v1 on collision."
-  [v1 v2]
-  (let [m2 (into {} (map (juxt :id identity) v2))]
-    (mapv #(merge % (get m2 (:id %))) v1)))
-
-
-(defn add-allocation-considered-count
-  "Takes a seq of entitlement maps and adds
-   :allocation_considered_count = item_count - allocations_in_other_entitlement_groups"
-  [entitlements]
-  (mapv (fn [e]
-
-          (let [
-
-
-                p (println ">o> abc" e)
-
-                e (assoc e :available_count (- (:items_count e)
-                                              (:allocations_in_other_entitlement_groups e)))
-
-
-
-                e (assoc e :is_quantity_ok (<= (:quantity e)
-                                              (:available_count e)))
-
-                p (println ">o> abc.quantity ???" (:quantity e) (:available_count e) (:is_quantity_ok e))
-                ;p (println ">o> abc.:available_count" (:available_count e))
-                ;p (println ">o> abc.:is_quantity_ok" (:is_quantity_ok e))
-
-
-
-                e (dissoc e :entitlement_group_id :allocations_in_other_entitlement_groups)
-
-                ] e)
-
-          )
-    entitlements))
-
-
-
-
-(defn join-by
-  "Performs a fast left join between two collections of maps based on a shared key.
-   Keeps all maps from coll-a, merging in matches from coll-b.
-   Example: (join-by :model_id vec-a vec-b)"
-  [k coll-a coll-b]
-  (let [b-index (into {} (map (juxt k identity)) coll-b)]
-    (mapv (fn [a]
-            (if-let [b (get b-index (get a k))]
-              (merge a b)
-              a))
-      coll-a)))
-
-
-
-
+;; -----------------------------
+;; HTTP Resource Handler
+;; -----------------------------
 
 (defn get-resource [request]
   (try
     (let [tx (:tx request)
-          pool_id (-> request path-params :pool_id)
+          pool-id (-> request path-params :pool_id)
           entitlement-group-id (-> request path-params :entitlement_group_id)
-          query (-> (sql/select :g.id, :g.name, :g.is_verification_required)
+
+          ;; 1️⃣ Fetch entitlement group
+          query (-> (sql/select :g.id :g.name :g.is_verification_required)
                   (sql/from [:entitlement_groups :g])
                   (sql/join [:inventory_pools :ip] [:= :g.inventory_pool_id :ip.id])
-                  (sql/where [:= :g.inventory_pool_id pool_id])
-                  (sql/where [:= :g.id entitlement-group-id])
+                  (sql/where [:and
+                              [:= :g.inventory_pool_id pool-id]
+                              [:= :g.id entitlement-group-id]])
                   (sql/order-by :g.name)
-                  sql-format
-                  )
-          entitlement_group (jdbc/execute-one! tx query)
+                  sql-format)
+          entitlement-group (jdbc/execute-one! tx query)
 
-
-
-          query (-> (sql/select :egu.id
-                      :egu.type
-                      :u.firstname
-                      :u.lastname
-                      :u.email
-                      :u.searchable)
+          ;; 2️⃣ Fetch users in entitlement group
+          query (-> (sql/select :egu.id :egu.type :u.firstname :u.lastname :u.email :u.searchable)
                   (sql/from [:entitlement_groups_users :egu])
                   (sql/join [:users :u] [:= :egu.user_id :u.id])
                   (sql/where [:= :egu.entitlement_group_id entitlement-group-id])
                   sql-format)
           users-groups (jdbc/execute! tx query)
 
-
-
-
-
-          ;(require '[next.jdbc :as jdbc])
-
+          ;; 3️⃣ Fetch models linked to entitlement group
           query (-> (sql/select
                       [:m.id :model_id]
                       :m.name
@@ -256,101 +161,32 @@
                   (sql/join [:models :m] [:= :e.model_id :m.id])
                   (sql/where [:= :e.entitlement_group_id entitlement-group-id])
                   sql-format)
-          res (jdbc/execute! tx query)
-          models res
-          p (println ">o> abc.models" res)
-          ;model_ids (filter #(#{"model_id"}
-          ;                 (:type %))
-          ;            res)
+          models (jdbc/execute! tx query)
+          model-ids (mapv :model_id models)
 
-          model_ids (mapv :model_id models)
+          ;; 4️⃣ Fetch item counts per model
+          model-ids (to-uuid model-ids)
+          models2 (select-entitlements-with-item-count tx pool-id model-ids entitlement-group-id)
 
-          p (println ">o> abc.model_ids" model_ids)
+          ;; 5️⃣ Join models + counts, compute availability
+          models3 (->> (join-by :model_id models models2)
+                    add-allocation-considered-count)
 
-
-
-
-          ;p (println ">o> abc.users-groups" users-groups)
-
-          groups (filter #(#{"group_entitlement"}
-                           (:type %))
-                   users-groups)
-          users (filter #(#{"direct_entitlement"}
-                          (:type %))
-                  users-groups)
-
-          p (println ">o> abc.start")
-
-
-          query (-> (sql/select :egu.id
-                      :egu.type
-                      :u.firstname
-                      :u.lastname
-                      :u.email
-                      :u.searchable)
-                  (sql/from [:entitlement_groups_users :egu])
-                  (sql/join [:users :u] [:= :egu.user_id :u.id])
-                  (sql/where [:= :egu.entitlement_group_id entitlement-group-id])
-                  sql-format)
-          users-groups (jdbc/execute! tx query)
-
-
-
-
-
-          p (println ">o> abc.start2")
-          models2 nil
-          models-count (try
-                         (let [
-                               p (println ">o> abc.model_ids??" model_ids)
-                               model_ids (to-uuid model_ids)
-
-                               res (select-entitlements-with-item-count tx
-                                     pool_id
-                                     model_ids
-                                     entitlement-group-id)
-
-                               ;models2 (add-allocation-considered-count res)
-                               ;p (println ">o> abc.sel-count" models2)
-                               ] res)
-
-                         (catch Exception e (println e))
-                         )
-
-          p (println "\n>o> abc.end.count" models-count)
-          p (println "\n>o> abc.end.models" models)
-          models2 models-count
-
-          models3 (join-by :model_id models models2)
-
-          models3 (add-allocation-considered-count models3)
-          p (println ">o> abc.sel-count" models3)
-
-
-          query (-> (sql/select
-                      :egg.id
-                      :egg.group_id
-                      :g.name
-                      :g.searchable
-                      )
+          ;; 6️⃣ Fetch linked groups
+          query (-> (sql/select :egg.id :egg.group_id :g.name :g.searchable)
                   (sql/from [:entitlement_groups_groups :egg])
                   (sql/join [:groups :g] [:= :egg.group_id :g.id])
                   (sql/where [:= :egg.entitlement_group_id entitlement-group-id])
                   sql-format)
           groups (jdbc/execute! tx query)
 
-
-          result {
-                  :entitlement-group entitlement_group
-                  ;:users users
-                  ;:groups groups
-                  :models models
-                  :models2 models2
-                  :models3 models3
-                  }
-          ]
+          result {:entitlement-group entitlement-group
+                  :users users-groups
+                  :groups groups
+                  :models models3}]
 
       (response result))
+
     (catch Exception e
-      (println e)
+      (error e "Error fetching entitlement group")
       (exception-handler request ERROR_GET e))))
