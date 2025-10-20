3d489ac0ce8d4597a3533cfa6bb7ca1e1a21af75
mradl <manuel.radl@zhdk.ch>
2025-10-20 11:14:28 +0200

    chore: revise query, missing attr

--- src/leihs/inventory/server/resources/pool/entitlement_groups/main.clj
+++ src/leihs/inventory/server/resources/pool/entitlement_groups/main.clj
@@ -5,12 +5,14 @@
    [honey.sql :refer [format] :rename {format sql-format}]
    [honey.sql.helpers :as sql]
    [leihs.inventory.server.resources.pool.entitlement-groups.common :refer [create-entitlements
-                                                                            link-users-to-entitlement-group
-                                                                            link-groups-to-entitlement-group]]
+                                                                            link-groups-to-entitlement-group
+                                                                            link-users-to-entitlement-group]]
+   [leihs.inventory.server.resources.pool.entitlement-groups.entitlement-group.query :refer [enrich-with-is-quantity-ok]]
    [leihs.inventory.server.utils.converter :refer [to-uuid]]
    [leihs.inventory.server.utils.exception-handler :refer [exception-handler]]
+   [leihs.inventory.server.utils.helper :refer [log-by-severity]]
    [leihs.inventory.server.utils.pagination :refer [create-pagination-response]]
-   [leihs.inventory.server.utils.request-utils :refer [path-params body-params]]
+   [leihs.inventory.server.utils.request-utils :refer [body-params path-params]]
    [next.jdbc :as jdbc]
    [leihs.inventory.server.utils.debug :refer [log-by-severity]]
    [leihs.inventory.server.utils.exception-handler :refer [exception-handler]]
@@ -19,44 +21,41 @@
    [ring.middleware.accept]
    [ring.util.response :refer [response]]))
 
-(defn- prep-query [ids]
-  (sql-format
-   {:raw
-    (str
-     "SELECT
-               eg.id,
-               eg.name,
-               eg.is_verification_required,
-               COALESCE(m.number_of_models, 0) AS number_of_models,
-               COALESCE(u.number_of_users, 0) AS number_of_users,
-               COALESCE(u.number_of_direct_users, 0) AS number_of_direct_users,
-               COALESCE(g.number_of_groups, 0) AS number_of_groups
-            FROM entitlement_groups eg
-            LEFT JOIN (
-                SELECT entitlement_group_id, COUNT(id) AS number_of_models
-                FROM entitlements
-                GROUP BY entitlement_group_id
-            ) m ON m.entitlement_group_id = eg.id
-            LEFT JOIN (
-                SELECT entitlement_group_id,
-                       COUNT(id) AS number_of_users,
-                       SUM(CASE WHEN type IN ('direct_entitlement', 'mixed')
-                                THEN 1 ELSE 0 END) AS number_of_direct_users
-                FROM entitlement_groups_users
-                GROUP BY entitlement_group_id
-            ) u ON u.entitlement_group_id = eg.id
-            LEFT JOIN (
-                SELECT entitlement_group_id, COUNT(id) AS number_of_groups
-                FROM entitlement_groups_groups
-                GROUP BY entitlement_group_id
-            ) g ON g.entitlement_group_id = eg.id
-            WHERE eg.id IN ("
-     (->> ids
-          (map #(str "'" % "'"))
-          (clojure.string/join ", "))
-     ");")}))
-
 (def ERROR_GET "Failed to get entitlement-groups")
+
+(defn- enrich-with-stats [tx ids]
+  (let [m-subquery (-> (sql/select :entitlement_group_id
+                                   [[:count :id] :number_of_models])
+                       (sql/from :entitlements)
+                       (sql/group-by :entitlement_group_id))
+        u-subquery (-> (sql/select :entitlement_group_id
+                                   [[:count :id] :number_of_users]
+                                   [[:sum [:cast
+                                           [:case
+                                            [:in :type ["direct_entitlement" "mixed"]] 1
+                                            :else 0]
+                                           :integer]]
+                                    :number_of_direct_users])
+                       (sql/from :entitlement_groups_users)
+                       (sql/group-by :entitlement_group_id))
+        g-subquery (-> (sql/select :entitlement_group_id
+                                   [[:count :id] :number_of_groups])
+                       (sql/from :entitlement_groups_groups)
+                       (sql/group-by :entitlement_group_id))
+        query (-> (sql/select :eg.id
+                              :eg.name
+                              :eg.is_verification_required
+                              [[:coalesce :m.number_of_models 0] :number_of_models]
+                              [[:coalesce :u.number_of_users 0] :number_of_users]
+                              [[:coalesce :u.number_of_direct_users 0] :number_of_direct_users]
+                              [[:coalesce :g.number_of_groups 0] :number_of_groups])
+                  (sql/from [:entitlement_groups :eg])
+                  (sql/left-join [m-subquery :m] [:= :m.entitlement_group_id :eg.id])
+                  (sql/left-join [u-subquery :u] [:= :u.entitlement_group_id :eg.id])
+                  (sql/left-join [g-subquery :g] [:= :g.entitlement_group_id :eg.id])
+                  (sql/where [:in :eg.id ids])
+                  sql-format)]
+    (jdbc/execute! tx query)))
 
 (defn- merge-by-id
   "Merge two vectors of maps by matching :id.
@@ -87,8 +86,9 @@
           post-fnc (fn [models]
                      (if (seq models)
                        (let [ids (to-uuid (mapv :id models))
-                             result (jdbc/execute! tx (prep-query ids))]
-                         (merge-by-id models result))
+                             models (merge-by-id models (enrich-with-is-quantity-ok tx pool_id ids))
+                             result (merge-by-id models (enrich-with-stats tx ids))]
+                         result)
                        []))]
       (response (create-pagination-response request query nil post-fnc)))
     (catch Exception e
@@ -113,4 +113,5 @@
                  :users users
                  :groups groups}))
     (catch Exception e
+      (log-by-severity ERROR_GET e)
       (exception-handler request ERROR_GET e))))
