58e22d191ee8a6d63e71d01d1310597070c4e9cf
mradl <manuel.radl@zhdk.ch>
2025-11-06 11:43:25 +0100

    cleanup

--- src/leihs/inventory/server/resources/pool/items/filter_handler.clj
+++ src/leihs/inventory/server/resources/pool/items/filter_handler.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure.data.json :as json]
    [clojure.string :as str]
-   [clojure.set :as set]               ;; ✅ added this
-
+   [clojure.set :as set]
    [honey.sql :refer [format] :rename {format sql-format}]
    [honey.sql.helpers :as sql]))
 
@@ -14,97 +13,21 @@
     (catch Exception _
       (throw (ex-info "Error during json-parse process." {:status 400})))))
 
-;; detect date-like string (yyyy-mm-dd)
 (defn date-string? [s]
   (and (string? s)
     (re-matches #"\d{4}-\d{2}-\d{2}" s)))
 
-;; improved range detection:
-;; only treat as BETWEEN if both elements are numbers or date strings
 (defn between-range? [v]
   (and (vector? v)
     (= 2 (count v))
     (or (every? number? v)
       (every? date-string? v))))
 
-;; vector with more than 2 elements, or 2 non-range elements
 (defn multi-value? [v]
   (and (vector? v)
     (or (> (count v) 2)
       (and (= 2 (count v))
         (not (between-range? v))))))
-
-;(defn add-filter
-;  "Add a single condition to the base query."
-;  [query [k v]]
-;  (cond
-;    ;; between range [from,to]
-;    (between-range? v)
-;    (let [[from to] v]
-;      (cond-> query
-;        (and from to) (sql/where [:between k from to])
-;        (and (nil? from) to) (sql/where [:<= k to])
-;        (and from (nil? to)) (sql/where [:>= k from])))
-;
-;    ;; multiple selection ["a","b","c"]
-;    (multi-value? v)
-;    (cond-> query (seq v) (sql/where [:in k v]))
-;
-;    ;; booleans, strings, numbers
-;    :else
-;    (cond-> query (some? v) (sql/where [:= k v]))))
-;
-;(defn add-filter
-;  "Add a single condition to the base query, wrapping each subexpression."
-;  [query [k v]]
-;  (cond
-;    ;; between range [from,to]
-;    (between-range? v)
-;    (let [[from to] v]
-;      (cond-> query
-;        (and from to) (sql/where [:and [:between k from to]])
-;        (and (nil? from) to) (sql/where [:and [:<= k to]])
-;        (and from (nil? to)) (sql/where [:and [:>= k from]])))
-;
-;    ;; multiple selection ["a","b","c"]
-;    (multi-value? v)
-;    (cond-> query
-;      (seq v) (sql/where [:and [:in k v]]))
-;
-;    ;; booleans, strings, numbers
-;    :else
-;    (cond-> query
-;      (some? v) (sql/where [:and [:= k v]]))))
-;
-;
-;(defn add-filter
-;  "Add a single condition to the base query, wrapping each subexpression."
-;  [query [k v]]
-;  (cond
-;    ;; special case: retired is a timestamp column, not boolean
-;    (= k :retired)
-;    (cond
-;      (= true v)  (sql/where query [:is-not k nil])
-;      (= false v) (sql/where query [:is k nil])
-;      :else       query)
-;
-;    ;; between range [from,to]
-;    (between-range? v)
-;    (let [[from to] v]
-;      (cond-> query
-;        (and from to) (sql/where [:and [:between k from to]])
-;        (and (nil? from) to) (sql/where [:and [:<= k to]])
-;        (and from (nil? to)) (sql/where [:and [:>= k from]])))
-;
-;    ;; multiple selection ["a","b","c"]
-;    (multi-value? v)
-;    (cond-> query
-;      (seq v) (sql/where [:and [:in k v]]))
-;
-;    ;; normal equals
-;    :else
-;    (cond-> query
-;      (some? v) (sql/where [:and [:= k v]]))))
 
 (defn uuid-string?
   "Returns true if v looks like a UUID string."
@@ -126,30 +49,22 @@
   "Add a single condition to the base query with type-sensitive handling."
   [query [k v]]
   (cond
-    ;; special case: retired (timestamp column)
     (= k :retired)
     (cond
       (= true v)  (sql/where query [:is-not k nil])
       (= false v) (sql/where query [:is k nil])
       :else       query)
 
-    ;; between range [from,to]
-    (between-range? v)
-    (let [[from to] v]
+    (between-range? v)    (let [[from to] v]
       (cond-> query
         (and from to) (sql/where [:and [:between k from to]])
         (and (nil? from) to) (sql/where [:and [:<= k to]])
         (and from (nil? to)) (sql/where [:and [:>= k from]])))
 
-    ;; multiple selection ["a","b","c"]
-    (multi-value? v)
-    (let [vals (cast-uuid v)]
+    (multi-value? v)    (let [vals (cast-uuid v)]
       (cond-> query
         (seq vals) (sql/where [:and [:in k vals]])))
-
-    ;; single value
-    :else
-    (let [val (cast-uuid v)]
+    :else    (let [val (cast-uuid v)]
       (cond-> query
         (some? val) (sql/where [:and [:= k val]])))))
 
