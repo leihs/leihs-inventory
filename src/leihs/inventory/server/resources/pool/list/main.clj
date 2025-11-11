5128449bb2df89781eb4894d30908121de3ec2d1
mradl <manuel.radl@zhdk.ch>
2025-11-11 10:29:23 +0100

    wip v2

--- src/leihs/inventory/server/resources/pool/list/main.clj
+++ src/leihs/inventory/server/resources/pool/list/main.clj
@@ -10,8 +10,10 @@
    [honey.sql.helpers :as sql]
    [leihs.core.core :refer [presence]]
    [leihs.inventory.server.resources.pool.fields.main :refer [fetch-properties-fields]]
-   [leihs.inventory.server.resources.pool.list.filter-handler :as filter]
-   [leihs.inventory.server.resources.pool.list.helper :as helper]
+   [leihs.inventory.server.resources.pool.list.filter-handler :as filter :refer [parse-json-param
+                                                               validate-filters
+                                                                                 add-filter-groups]]
+   [leihs.inventory.server.resources.pool.list.helper :as helper :refer [extract-ids prepare-filters]]
    [leihs.inventory.server.resources.pool.list.export-csv :as export-csv]
    [leihs.inventory.server.resources.pool.list.export-excel :as export-excel]
    [leihs.inventory.server.resources.pool.models.common :refer [fetch-thumbnails-for-ids
@@ -37,6 +39,7 @@
       )
     (sql/right-join :items [:= :items.model_id :inventory.id])))
 
+
 (defn index-resources
   ([request]
    (let [tx (:tx request)
@@ -48,22 +51,12 @@
                  search before_last_check filters]} (query-params request)
 
          accept-type (-> request :accept :mime)
-         p (println ">o> abc.accept-type2" accept-type)
-
-         p (println ">o> abc.before, filters" filters)
-         parsed-filters (filter/parse-json-param filters)
-         p (println ">o> abc.after, filters" parsed-filters)
-
+         parsed-filters (parse-json-param filters)
          properties-fields (fetch-properties-fields request)
-         p (println ">o> abc.properties-fields.count" (first properties-fields))
-
-         {:keys [filter-keys properties raw-filter-keys]} (helper/extract-ids properties-fields "properties_")
+         {:keys [filter-keys properties raw-filter-keys]} (extract-ids properties-fields "properties_")
          WHITELIST-ITEM-FILTER filter-keys
-         p (println ">o> abc.WHITELIST-ITEM-FILTER2" WHITELIST-ITEM-FILTER)
-
-         parsed-filters (helper/prepare-filters parsed-filters)
-         p (println ">o> abc.parsed-filters" parsed-filters)
-         validation-result (filter/validate-filters parsed-filters WHITELIST-ITEM-FILTER)
+         parsed-filters (prepare-filters parsed-filters)
+         validation-result (validate-filters parsed-filters WHITELIST-ITEM-FILTER)
 
          query (-> (base-inventory-query pool-id)
                  (cond-> type (filter-by-type type))
@@ -78,17 +71,14 @@
                                 :broken broken
                                 :inventory_pool_id inventory_pool_id
                                 :owned owned
-                                :in_stock in_stock
-                                }
+                                :in_stock in_stock}
                          (not= type :software)
                          (assoc :before_last_check before_last_check)))
                      (and pool-id (false? with_items))
                      (without-items pool-id)))
-
                  (cond-> (seq parsed-filters)
                    (-> items-sub-query
-                     (filter/add-filter-groups parsed-filters raw-filter-keys)))
-
+                     (add-filter-groups parsed-filters raw-filter-keys)))
                  (cond-> (and pool-id (presence search))
                    (with-search search))
                  (cond-> (and category_id (not (some #{type} [:option :software])))
@@ -98,9 +88,13 @@
                     (->> models
                       (fetch-thumbnails-for-ids tx)
                       (map (model->enrich-with-image-attr pool-id))))]
-     (println ">o> ???" (sql-format query :inline true))
 
      (cond
-       (= accept-type :csv) (export-csv/convert (create-pagination-response request query false post-fnc))
-       (= accept-type :excel) (export-excel/convert (create-pagination-response request query false post-fnc))
-       :else (response (create-pagination-response request query nil post-fnc))))))
+       (= accept-type :csv)
+       (export-csv/convert (create-pagination-response request query false post-fnc))
+
+       (= accept-type :excel)
+       (export-excel/convert (create-pagination-response request query false post-fnc))
+
+       :else
+       (response (create-pagination-response request query nil post-fnc))))))
