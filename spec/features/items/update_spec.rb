3bcb8fd9cfe8b0c8c9d7b248e25c320e0540f6bc
Jan Pistor <pistor.jan@gmail.com>
2025-11-26 16:52:50 +0100

    chore: tests

--- spec/features/items/update_spec.rb
+++ spec/features/items/update_spec.rb
@@ -205,7 +205,8 @@ feature "Update item", type: :feature do
     fill_in "Shelf", with: shelf_new
 
     # Retry clicking Save until success message appears
-    click_on_until("Save", expect: "Item was successfully saved")
+    # click_on_until("Save", expect: "Item was successfully saved")
+    click_on "Save"
     expect(page).to have_text("Inventory List")
 
     fill_in "search", with: model_new.product
--- src/leihs/inventory/client/routes/pools/items/crud/page.cljs
+++ src/leihs/inventory/client/routes/pools/items/crud/page.cljs
@@ -232,7 +232,6 @@
 
                   ($ Form (merge form)
                      ($ :form {:id "create-model"
-                               :no-validate true
                                :className "space-y-12 w-full lg:w-3/5"
                                :on-submit (handle-submit on-submit on-invalid)}
 
@@ -251,51 +250,44 @@
                                                      :form form
                                                      :block block}))))))
 
-                  ($ Button {:type "submit"
-                             :form "create-model"
-                             :className "self-center"}
-                     (if is-create
-                       (t "pool.items.item.create.submit")
-                       (t "pool.items.item.edit.submit")))
+                  ($ StickyBottom {:class-name "ml-auto"
+                                   :offset "50px"}
+                     ($ ButtonGroup
+                        ($ Button {:type "submit"
+                                   :form "create-model"
+                                   :className "self-center"}
+                           (if is-create
+                             (t "pool.items.item.create.submit")
+                             (t "pool.items.item.edit.submit")))
+                        ($ ButtonGroupSeparator)
+                        ($ DropdownMenu
+                           ($ DropdownMenuTrigger {:asChild true}
+                              ($ Button {:data-test-id "submit-dropdown"
+                                         :className "self-center !px-2"}
+                                 ($ ChevronDownIcon)))
 
-                  #_($ StickyBottom {:class-name "ml-auto"
-                                     :offset "50px"}
-                       ($ ButtonGroup
-                          ($ Button {:type "submit"
-                                     :form "create-model"
-                                     :className "self-center"}
-                             (if is-create
-                               (t "pool.items.item.create.submit")
-                               (t "pool.items.item.edit.submit")))
-                          ($ ButtonGroupSeparator)
-                          ($ DropdownMenu
-                             ($ DropdownMenuTrigger {:asChild true}
-                                ($ Button {:data-test-id "submit-dropdown"
-                                           :className "self-center !px-2"}
-                                   ($ ChevronDownIcon)))
-
-                             ($ DropdownMenuContent {:align "end"
-                                                     :class-name "[--radius:1rem]"}
-                                ($ DropdownMenuGroup
-                                   ($ DropdownMenuItem
-                                      {:asChild true}
-                                      ($ Link {:to (str (router/generatePath "/inventory/:pool-id/list" params)
-                                                        (some-> state .-searchParams))
-                                               :viewTransition true}
-                                         (if is-create
-                                           (t "pool.items.item.create.cancel")
-                                           (t "pool.items.item.edit.cancel")))))
+                           ($ DropdownMenuContent {:align "end"
+                                                   :class-name "[--radius:1rem]"}
+                              ($ DropdownMenuGroup
+                                 ($ DropdownMenuItem
+                                    {:asChild true}
+                                    ($ Link {:to (str (router/generatePath "/inventory/:pool-id/list" params)
+                                                      (some-> state .-searchParams))
+                                             :viewTransition true}
+                                       (if is-create
+                                         (t "pool.items.item.create.cancel")
+                                         (t "pool.items.item.edit.cancel")))))
 
                               ;; prepared for "ausmustern"
-                                #_($ DropdownMenuSeparator)
+                              #_($ DropdownMenuSeparator)
 
-                                #_($ DropdownMenuGroup
-                                     (when (not is-create)
-                                       ($ DropdownMenuItem {:variant "destructive"
-                                                            :asChild true}
-                                          ($ Link {:to (router/generatePath "/inventory/:pool-id/items/:item-id/delete" params)
-                                                   :state state}
-                                             "Delete"))))))))
+                              #_($ DropdownMenuGroup
+                                   (when (not is-create)
+                                     ($ DropdownMenuItem {:variant "destructive"
+                                                          :asChild true}
+                                        ($ Link {:to (router/generatePath "/inventory/:pool-id/items/:item-id/delete" params)
+                                                 :state state}
+                                           "Delete"))))))))
 
                   ;; prepared for "ausmustern"
                   #_(when (not is-create)
