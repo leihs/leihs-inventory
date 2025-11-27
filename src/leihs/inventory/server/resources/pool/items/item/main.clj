decf71e01a510171f1af9b9e1e557cd46843c6eb
Matus Kmit <matuskmit1@gmail.com>
2025-11-27 12:25:52 +0100

    wip

--- src/leihs/inventory/server/resources/pool/items/item/main.clj
+++ src/leihs/inventory/server/resources/pool/items/item/main.clj
@@ -11,13 +11,19 @@
                                                                       validate-field-permissions]]
    [leihs.inventory.server.utils.debug :refer [log-by-severity]]
    [leihs.inventory.server.utils.exception-handler :refer [exception-handler]]
-   [leihs.inventory.server.utils.request-utils :refer [body-params path-params]]
    [next.jdbc :as jdbc]
    [ring.middleware.accept]
-   [ring.util.response :refer [bad-request response status]]
+   [ring.util.response :refer [bad-request not-found response status]]
    [taoensso.timbre :refer [debug spy]]))
 
 (def ERROR_UPDATE_ITEM "Failed to update item")
+
+(defn get-one [tx item-id]
+  (-> (sql/select :*)
+      (sql/from :items)
+      (sql/where [:= :items.id item-id])
+      sql-format
+      (->> (jdbc/execute-one! tx))))
 
 (defn inventory-code-exists? [tx inventory-code exclude-id]
   (let [query (-> (sql/select :id)
@@ -29,35 +35,38 @@
     (-> (jdbc/execute-one! tx query)
         some?)))
 
-(defn patch-resource [{:keys [tx] :as request}]
+(defn patch-resource [{:keys [tx]
+                       {{:keys [item_id pool_id]} :path
+                        update-params :body} :parameters
+                       :as request}]
   (try
-    (if-let [validation-error (validate-field-permissions request)]
-      (bad-request validation-error)
-      (let [update-params (body-params request)
-            {:keys [item_id pool_id]} (path-params request)
-            {:keys [item-data properties]} (-> update-params (dissoc :id)
-                                               split-item-data)
-            inventory-code (:inventory_code item-data)]
-        (if (and inventory-code (inventory-code-exists? tx inventory-code item_id))
-          (status {:body {:error "Inventory code already exists"
-                          :proposed_code (inv-code/propose tx pool_id)}}
-                  409)
-          (let [item-data-coerced (coerce-field-values item-data in-coercions)
-                properties-json (or (not-empty properties) {})
-                item-data-with-properties (cond-> item-data-coerced
-                                            (contains? item-data-coerced :properties)
-                                            (assoc :properties [:lift properties-json]))
-                sql-query (-> (sql/update :items)
-                              (sql/set item-data-with-properties)
-                              (sql/where [:= :id item_id])
-                              (sql/returning :*)
-                              (sql-format :inline true))
-                result (jdbc/execute-one! tx sql-query)]
-            (if result
-              (response (-> result
-                            flatten-properties
-                            (coerce-field-values out-coercions)))
-              (bad-request {:error ERROR_UPDATE_ITEM}))))))
+    (if-let [item (get-one tx item_id)]
+      (if-let [validation-error (validate-field-permissions request)]
+        (bad-request validation-error)
+        (let [{:keys [item-data properties]} (-> update-params
+                                                 (dissoc :id)
+                                                 split-item-data)
+              inventory-code (:inventory_code item-data)]
+          (if (and inventory-code (inventory-code-exists? tx inventory-code item_id))
+            (status {:body {:error "Inventory code already exists"
+                            :proposed_code (inv-code/propose tx pool_id)}}
+                    409)
+            (let [item-data-coerced (coerce-field-values item-data in-coercions)
+                  properties-json (merge (:properties item) properties)
+                  item-data-with-properties (assoc item-data-coerced
+                                                   :properties [:lift properties-json])
+                  sql-query (-> (sql/update :items)
+                                (sql/set item-data-with-properties)
+                                (sql/where [:= :id item_id])
+                                (sql/returning :*)
+                                sql-format)
+                  result (jdbc/execute-one! tx sql-query)]
+              (if result
+                (response (-> result
+                              flatten-properties
+                              (coerce-field-values out-coercions)))
+                (bad-request {:error ERROR_UPDATE_ITEM}))))))
+      (not-found {:error "Item not found"}))
     (catch Exception e
       (log-by-severity ERROR_UPDATE_ITEM e)
       (exception-handler request ERROR_UPDATE_ITEM e))))
