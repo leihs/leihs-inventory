a17cbe0f4bf9754c095caa47720d82c9542510ff
Jan Pistor <pistor.jan@gmail.com>
2026-03-09 09:50:11 +0100

    feat: add license routing

--- src/leihs/inventory/client/loader.cljs
+++ src/leihs/inventory/client/loader.cljs
@@ -237,6 +237,36 @@
       (catch js/Error err
         (handle-error err)))))
 
+(defn licenses-crud-page [route-data]
+  (p/let [params (.. ^js route-data -params)
+          pool-id (aget params "pool-id")
+          license-id (or (aget params "license-id") nil)
+          software-id (or (aget params "software-id") nil)
+
+          model (when software-id
+                  (-> http-client
+                      (.get (str "/inventory/" pool-id "/software/" software-id))
+                      (.then #(jc (.-data %)))))
+
+          license (when license-id
+                    (-> http-client
+                        (.get (str "/inventory/" pool-id "/items/" license-id)
+                              #js {:cache false})
+                        (.then #(jc (.-data %)))))
+
+          data (if license-id
+                 {:fields (:fields license)}
+                 (-> http-client
+                     (.get (str "/inventory/" pool-id "/fields/?target_type=license")
+                           #js {:cache false})
+                     (.then #(jc (.-data %)))))]
+
+    (try
+      {:data data
+       :model model}
+      (catch js/Error err
+        (handle-error err)))))
+
 (defn models-crud-page [route-data]
   (p/let [params (.. ^js route-data -params)
           pool-id (aget params "pool-id")
