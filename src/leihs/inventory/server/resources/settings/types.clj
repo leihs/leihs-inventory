5c1e050c29ffdd6369f6a5aac8aedc2ab7244ebc
Jan Pistor <pistor.jan@gmail.com>
2026-04-28 14:23:35 +0200

    feat: logo from settings

--- src/leihs/inventory/server/resources/settings/types.clj
+++ src/leihs/inventory/server/resources/settings/types.clj
@@ -8,6 +8,8 @@
 (def response-schema
   {:user_image_url (s/maybe s/Str) ; Optional string
    :logo_url (s/maybe s/Str) ; Optional string
+   :logo_light (s/maybe s/Str) ; Optional string
+   :logo_dark (s/maybe s/Str) ; Optional string
    :local_currency_string (s/maybe s/Str) ; Optional string
    :time_zone s/Str ; Required string
    :lending_terms_url (s/maybe s/Str) ; Optional string
