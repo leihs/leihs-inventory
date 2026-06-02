b61d0efce0baa8622fb887197e86be4e02c8155d
Jan Pistor <pistor.jan@gmail.com>
2026-06-02 13:31:00 +0200

    fix: search edit item info

--- src/leihs/inventory/client/routes/pools/inventory/search_edit/components/table/item_info.cljs
+++ src/leihs/inventory/client/routes/pools/inventory/search_edit/components/table/item_info.cljs
@@ -3,6 +3,7 @@
    ["date-fns" :refer [format]]
    ["react-i18next" :refer [useTranslation]]
    ["react-router-dom" :as router]
+   [leihs.inventory.client.components.typo :refer [Typo]]
    [uix.core :as uix :refer [$ defui]]))
 
 (defui ItemInfo [{:keys [item]}]
@@ -15,11 +16,13 @@
           (:inventory_pool_name item))
        ($ :span
           (if (:reservation_user_name item)
-            ($ :a {:href (str "/manage/" pool-id "/contracts/" (:reservation_contract_id item))
-                   :target "_blank"
-                   :rel "noreferrer"}
-               (str (:reservation_user_name item) " " (t "pool.models.search_edit.info.until") " "
-                    (format (:reservation_end_date item) "dd.MM.yyyy")))
+            ($ Typo {:variant :link
+                     :class-name "text-muted-foreground"}
+               ($ :a {:href (str "/manage/" pool-id "/contracts/" (:reservation_contract_id item))
+                      :target "_blank"
+                      :rel "noreferrer"}
+                  (str (:reservation_user_name item) " until "
+                       (format (:reservation_end_date item) "dd.MM.yyyy"))))
 
             (str (:building_name item)
                  " ( " (:building_code item) " ) "
