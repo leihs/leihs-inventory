11f45232e3076a672e61aff37cefc88acc606502
uvb <urs.vanbinsbergen@zhdk.ch>
2026-08-27 11:21:09 +0200

    fix: checkbox field labels

--- src/leihs/inventory/client/components/form/fields/checkbox_group_field.cljs
+++ src/leihs/inventory/client/components/form/fields/checkbox_group_field.cljs
@@ -3,15 +3,18 @@
    ["@@/checkbox-group" :refer [CheckboxGroup CheckboxGroupItem]]
    ["@@/field" :refer [FieldSet FieldLegend]]
    ["@@/form" :refer [FormControl FormField FormMessage]]
+   ["react-i18next" :refer [useTranslation]]
    [uix.core :refer [$ defui]]))
 
 (defui CheckboxGroupField [{:keys [form block class-name]}]
-  (let [label (:label block)]
+  (let [[t] (useTranslation)
+        label (:label block)]
     ($ FormField {:control (.-control form)
                   :name (:name block)
                   :render #($ FieldSet {:class-name (str "mt-6 gap-0 " class-name)}
                               (when label
                                 ($ FieldLegend {:variant "label"}
+                                   label
                                    (when (-> block :props :required) "*")))
 
                               ($ FormControl
@@ -21,6 +24,8 @@
                                     (for [option (:options (:props block))]
                                       ($ CheckboxGroupItem {:key (:value option)
                                                             :value (:value option)
-                                                            :label (:label option)
+                                                            :label (if (:bypass-i18n (:props block))
+                                                                     (:label option)
+                                                                     (t (:label option)))
                                                             :data-test-id (str (:name block) "-" (:value option))}))))
                               ($ FormMessage))})))
